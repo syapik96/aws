6a6959b4556f14e93cc56fe585727d33838e4d85
syapik96 <79759525+syapik96@users.noreply.github.com>
2021-09-07 07:00:37 +0800

    Update addhost.sh

--- tambah/addhost.sh
+++ tambah/addhost.sh
@@ -1,41 +1,33 @@
 #!/bin/bash
-red='\e[1;31m'
-green='\e[0;32m'
-NC='\e[0m'
-MYIP=$(wget -qO- ipinfo.io/ip);
-echo "Checking VPS"
-apt install jq curl -y
-DOMAIN=gilerspeednet.tk
-sub=$(</dev/urandom tr -dc a-z0-9 | head -c4)
-SUB_DOMAIN=${sub}.gilerspeednet.tk
-CF_ID=zulhisyam421@gmail.com
-CF_KEY=48e94b491ba3933abe8732b9a5f91cf3f3c36
-set -euo pipefail
-IP=$(wget -qO- ipinfo.io/ip);
-echo "Updating DNS for ${SUB_DOMAIN}..."
-ZONE=$(curl -sLX GET "https://api.cloudflare.com/client/v4/zones?name=${DOMAIN}&status=active" \
-     -H "X-Auth-Email: ${CF_ID}" \
-     -H "X-Auth-Key: ${CF_KEY}" \
-     -H "Content-Type: application/json" | jq -r .result[0].id)
-
-RECORD=$(curl -sLX GET "https://api.cloudflare.com/client/v4/zones/${ZONE}/dns_records?name=${SUB_DOMAIN}" \
-     -H "X-Auth-Email: ${CF_ID}" \
-     -H "X-Auth-Key: ${CF_KEY}" \
-     -H "Content-Type: application/json" | jq -r .result[0].id)
-
-if [[ "${#RECORD}" -le 10 ]]; then
-     RECORD=$(curl -sLX POST "https://api.cloudflare.com/client/v4/zones/${ZONE}/dns_records" \
-     -H "X-Auth-Email: ${CF_ID}" \
-     -H "X-Auth-Key: ${CF_KEY}" \
-     -H "Content-Type: application/json" \
-     --data '{"type":"A","name":"'${SUB_DOMAIN}'","content":"'${IP}'","ttl":120,"proxied":false}' | jq -r .result.id)
-fi
-
-RESULT=$(curl -sLX PUT "https://api.cloudflare.com/client/v4/zones/${ZONE}/dns_records/${RECORD}" \
-     -H "X-Auth-Email: ${CF_ID}" \
-     -H "X-Auth-Key: ${CF_KEY}" \
-     -H "Content-Type: application/json" \
-     --data '{"type":"A","name":"'${SUB_DOMAIN}'","content":"'${IP}'","ttl":120,"proxied":false}')
-echo "Host : $SUB_DOMAIN"
-echo $SUB_DOMAIN > /root/domain
-rm -f /root/cf.sh
+YXB0IGluc3RhbGwganEgY3VybCAteQpET01BSU49a2FzaWhlbnRhbWFqZS5jZgpz
+dWI9JCg8L2Rldi91cmFuZG9tIHRyIC1kYyBhLXowLTkgfCBoZWFkIC1jNCkKU1VC
+X0RPTUFJTj0ke3N1Yn0ua2FzaWhlbnRhbWFqZS5jZgpDRl9JRD16dWxoaXN5YW00
+MjFAZ21haWwuY29tCkNGX0tFWT00OGU5NGI0OTFiYTM5MzNhYmU4NzMyYjlhNWY5
+MWNmM2YzYzM2CnNldCAtZXVvIHBpcGVmYWlsCklQPSQod2dldCAtcU8tIGlwaW5m
+by5pby9pcCk7CmVjaG8gIlVwZGF0aW5nIEROUyBmb3IgJHtTVUJfRE9NQUlOfS4u
+LiIKWk9ORT0kKGN1cmwgLXNMWCBHRVQgImh0dHBzOi8vYXBpLmNsb3VkZmxhcmUu
+Y29tL2NsaWVudC92NC96b25lcz9uYW1lPSR7RE9NQUlOfSZzdGF0dXM9YWN0aXZl
+IiBcCiAgICAgLUggIlgtQXV0aC1FbWFpbDogJHtDRl9JRH0iIFwKICAgICAtSCAi
+WC1BdXRoLUtleTogJHtDRl9LRVl9IiBcCiAgICAgLUggIkNvbnRlbnQtVHlwZTog
+YXBwbGljYXRpb24vanNvbiIgfCBqcSAtciAucmVzdWx0WzBdLmlkKQoKUkVDT1JE
+PSQoY3VybCAtc0xYIEdFVCAiaHR0cHM6Ly9hcGkuY2xvdWRmbGFyZS5jb20vY2xp
+ZW50L3Y0L3pvbmVzLyR7Wk9ORX0vZG5zX3JlY29yZHM/bmFtZT0ke1NVQl9ET01B
+SU59IiBcCiAgICAgLUggIlgtQXV0aC1FbWFpbDogJHtDRl9JRH0iIFwKICAgICAt
+SCAiWC1BdXRoLUtleTogJHtDRl9LRVl9IiBcCiAgICAgLUggIkNvbnRlbnQtVHlw
+ZTogYXBwbGljYXRpb24vanNvbiIgfCBqcSAtciAucmVzdWx0WzBdLmlkKQoKaWYg
+W1sgIiR7I1JFQ09SRH0iIC1sZSAxMCBdXTsgdGhlbgogICAgIFJFQ09SRD0kKGN1
+cmwgLXNMWCBQT1NUICJodHRwczovL2FwaS5jbG91ZGZsYXJlLmNvbS9jbGllbnQv
+djQvem9uZXMvJHtaT05FfS9kbnNfcmVjb3JkcyIgXAogICAgIC1IICJYLUF1dGgt
+RW1haWw6ICR7Q0ZfSUR9IiBcCiAgICAgLUggIlgtQXV0aC1LZXk6ICR7Q0ZfS0VZ
+fSIgXAogICAgIC1IICJDb250ZW50LVR5cGU6IGFwcGxpY2F0aW9uL2pzb24iIFwK
+ICAgICAtLWRhdGEgJ3sidHlwZSI6IkEiLCJuYW1lIjoiJyR7U1VCX0RPTUFJTn0n
+IiwiY29udGVudCI6Iicke0lQfSciLCJ0dGwiOjEyMCwicHJveGllZCI6ZmFsc2V9
+JyB8IGpxIC1yIC5yZXN1bHQuaWQpCmZpCgpSRVNVTFQ9JChjdXJsIC1zTFggUFVU
+ICJodHRwczovL2FwaS5jbG91ZGZsYXJlLmNvbS9jbGllbnQvdjQvem9uZXMvJHta
+T05FfS9kbnNfcmVjb3Jkcy8ke1JFQ09SRH0iIFwKICAgICAtSCAiWC1BdXRoLUVt
+YWlsOiAke0NGX0lEfSIgXAogICAgIC1IICJYLUF1dGgtS2V5OiAke0NGX0tFWX0i
+IFwKICAgICAtSCAiQ29udGVudC1UeXBlOiBhcHBsaWNhdGlvbi9qc29uIiBcCiAg
+ICAgLS1kYXRhICd7InR5cGUiOiJBIiwibmFtZSI6Iicke1NVQl9ET01BSU59JyIs
+ImNvbnRlbnQiOiInJHtJUH0nIiwidHRsIjoxMjAsInByb3hpZWQiOmZhbHNlfScp
+CmVjaG8gIkhvc3QgOiAkU1VCX0RPTUFJTiIKZWNobyAkU1VCX0RPTUFJTiA+IC9y
+b290L2RvbWFpbgpybSAtZiAvcm9vdC9jZi5zaA==
