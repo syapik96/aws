ecb051ee543763aa20118ba68e9361a8be6f5877
syapik96 <79759525+syapik96@users.noreply.github.com>
2021-09-09 05:44:54 +0800

    Update add-host.sh

--- add-host.sh
+++ add-host.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-read -rp "Domain/Host: " -e host
-echo "IP=$host">>/var/lib/crot-script/ipvps.conf
+read -rp "Domain/Hostname: " -e host
+echo "IP=$host">>/var/lib/premium-script/ipvps.conf
