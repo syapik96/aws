c503cc91b52713b4be6f665a43839b573233192b
syapik96 <79759525+syapik96@users.noreply.github.com>
2021-09-09 15:55:19 +0800

    Update add-host.sh

--- add-host.sh
+++ add-host.sh
@@ -1,3 +1,46 @@
 #!/bin/bash
-read -rp "Domain/Hostname: " -e host
-echo "IP=$host">>/var/lib/premium-script/ipvps.conf
+
+red='\e[1;31m'
+green='\e[0;32m'
+NC='\e[0m'
+
+clear
+
+read -rp "Masukkan Domain: " -e DOMAIN
+echo ""
+echo "Domain: ${DOMAIN}" 
+echo ""
+read -rp "Masukkan Subdomain: " -e sub
+SUB_DOMAIN=${sub}.${DOMAIN}
+CF_ID=bXVoYW1tYWQubmVpemFtQGdtYWlsLmNvbQ
+CF_KEY=MTBkYTcyNjQ4MDU4NGE2MDFlY2M1OGIxMjYyMWYwYjAxMDJlYw
+set -euo pipefail
+IP=$(wget -qO- http://ipecho.net/plain);
+echo "Pointing DNS Untuk Domain ${SUB_DOMAIN}..."
+ZONE=$(curl -sLX GET "https://api.cloudflare.com/client/v4/zones?name=${DOMAIN}&status=active" \
+     -H "X-Auth-Email: ${CF_ID}" \
+     -H "X-Auth-Key: ${CF_KEY}" \
+     -H "Content-Type: application/json" | jq -r .result[0].id)
+
+RECORD=$(curl -sLX GET "https://api.cloudflare.com/client/v4/zones/${ZONE}/dns_records?name=${SUB_DOMAIN}" \
+     -H "X-Auth-Email: ${CF_ID}" \
+     -H "X-Auth-Key: ${CF_KEY}" \
+     -H "Content-Type: application/json" | jq -r .result[0].id)
+
+if [[ "${#RECORD}" -le 10 ]]; then
+     RECORD=$(curl -sLX POST "https://api.cloudflare.com/client/v4/zones/${ZONE}/dns_records" \
+     -H "X-Auth-Email: ${CF_ID}" \
+     -H "X-Auth-Key: ${CF_KEY}" \
+     -H "Content-Type: application/json" \
+     --data '{"type":"A","name":"'${SUB_DOMAIN}'","content":"'${IP}'","ttl":120,"proxied":false}' | jq -r .result.id)
+fi
+
+RESULT=$(curl -sLX PUT "https://api.cloudflare.com/client/v4/zones/${ZONE}/dns_records/${RECORD}" \
+     -H "X-Auth-Email: ${CF_ID}" \
+     -H "X-Auth-Key: ${CF_KEY}" \
+     -H "Content-Type: application/json" \
+     --data '{"type":"A","name":"'${SUB_DOMAIN}'","content":"'${IP}'","ttl":120,"proxied":false}')
+echo "Host : $SUB_DOMAIN"
+echo "$SUB_DOMAIN > /root/domain"
+echo "$SUB_DOMAIN > /etc/v2ray/domain"
+echo "$IP > /var/lib/premium-script/ipvps.conf"
