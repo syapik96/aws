668911cd71c448f762397ae799414fd27b24cc02
syapik96 <79759525+syapik96@users.noreply.github.com>
2021-09-17 22:41:06 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,17 +1,13 @@
 #!/bin/bash
-# Adjusted by PrinceNewbie
-# Updated , Copyright 2021 ,    
-# SILA BACA TEXT BERSIMBOL # 
-# INI BAGI MEMUDAHKAN BG YANG INGIN MENGGUNAKAN SKRIP INI
-# ==================================================
-
-GitUser="syapik96"
-#wget https://github.com/${GitUser}/
+# Script Updated , Copyright * 2021
+# Premium Script by t.me/PrinceNewbie
 if [ "${EUID}" -ne 0 ]; then
 echo "You need to run this script as root"
 exit 1
-red='\e[1;31m'
-green='\e[0;32m'
+fi
+RED='\e[1;31m'
+GREEN='\e[0;32m'
+BLUE='\e[0;34m'
 NC='\e[0m'
 
 clear
@@ -21,17 +17,9 @@ sysctl -w net.ipv6.conf.default.disable_ipv6 = 1
 timedatectl set-timezone 'Asia/Kuala_Lumpur'
 apt-get update && apt-get upgrade -y 
 apt install -y bzip2 gzip coreutils screen curl
-apt-get install figlet
-apt-get install ruby
-gem install lolcat
-apt -y install wget curl
-clear
 
-MYIP=$(wget -qO- icanhazip.com);  
-mkdir /etc/v2ray
 mkdir /var/lib/premium-script;
-#echo "${MYIP}" >> /var/lib/premium-script/ipvps.conf
-#echo "${host}" >> /etc/v2ray/domain
+echo "IP=" >> /var/lib/premium-script/ipvps.conf
 
 GitUser="syapik96"
 #wget https://github.com/${GitUser}/
@@ -56,56 +44,88 @@ rm -f /root/shadowsocksobfs.sh
 rm -f /root/ins-vt.sh
 rm -f /root/go.sh
 rm -f /root/ipsec.sh
-history -c 
 
-echo "1.1" > /home/ver
+sudo apt-get install figlet
+sudo apt-get install ruby
+sudo gem install lolcat
+
+cat <<EOF > /etc/systemd/system/autosett.service
+[Unit]
+Description=autosetting
+Documentation=https://trickinternetvpns.ml
+[Service]
+Type=oneshot
+ExecStart=/bin/bash /etc/set.sh
+RemainAfterExit=yes
+[Install]
+WantedBy=multi-user.target
+EOF
+systemctl daemon-reload
+systemctl enable autosett
+
+wget https://raw.githubusercontent.com/${GitUser}/aws/main/install.sh && chmod +x install.sh && ./install.sh
+
+history -c
+echo "1.1.1" > /home/ver
 clear
+figlet -f slant OnePieceVPN | lolcat
 echo " "
-echo "Installation has been completed!!"
 echo " "
-echo "===========================-Premium-Script by OnePieceVPN-Project-=========================" | tee -a log-install.txt
+echo "=================================-[ AUTOSCRIPT PREMIUM ]-===========================" | tee -a log-install.txt
 echo "" | tee -a log-install.txt
-echo "----------------------------------------------------------------------------------" | tee -a log-install.txt
-echo ""  | tee -a log-install.txt
-echo "   >>> Service & Port"  | tee -a log-install.txt
-echo "   - OpenSSH                  : 22, 500"  | tee -a log-install.txt
-echo "   - SSH-WS Python OpenSSH    : 100"  | tee -a log-install.txt
-echo "   - SSH-WS Python Dropbear   : 2020, 2021"  | tee -a log-install.txt
-echo "   - SSH-WS Python SSL/TLS    : 5052, 5053"  | tee -a log-install.txt
-echo "   - OpenVPN-WS Python        : 6969"  | tee -a log-install.txt
-echo "   - OpenVPN                  : TCP 1194, UDP 2200, SSL 992"  | tee -a log-install.txt
-echo "   - Stunnel4 SSL/TLS         : 443"  | tee -a log-install.txt
-echo "   - Dropbear                 : 143, 109"  | tee -a log-install.txt
-echo "   - Squid Proxy              : 3128, 8080 (limit to IP Server)"  | tee -a log-install.txt
-echo "   - Badvpn                   : 7100, 7200, 7300"  | tee -a log-install.txt
-echo "   - Nginx                    : 81"  | tee -a log-install.txt
-echo "   - Wireguard                : 7070"  | tee -a log-install.txt
-echo "   - L2TP/IPSEC VPN           : 1701"  | tee -a log-install.txt
-echo "   - PPTP VPN                 : 1732"  | tee -a log-install.txt
-echo "   - SSTP VPN                 : 5555"  | tee -a log-install.txt
-echo "   - Shadowsocks-R            : 1443-1543"  | tee -a log-install.txt
-echo "   - SS-OBFS TLS              : 2443-2543"  | tee -a log-install.txt
-echo "   - SS-OBFS HTTP             : 3443-3453"  | tee -a log-install.txt
-echo "   - V2RAY Vmess TLS          : 4443"  | tee -a log-install.txt
-echo "   - V2RAY Vmess None TLS     : 80"  | tee -a log-install.txt
-echo "   - Trojan                   : 6443"  | tee -a log-install.txt
-echo ""  | tee -a log-install.txt
-echo "   >>> Server Information & Other Features"  | tee -a log-install.txt
-echo "   - Timezone                : Asia/Kuala_Lumpur (GMT +8)"  | tee -a log-install.txt
-echo "   - Fail2Ban                : [ON]"  | tee -a log-install.txt
-echo "   - Dflate                  : [ON]"  | tee -a log-install.txt
-echo "   - IPtables                : [ON]"  | tee -a log-install.txt
-echo "   - Auto-Reboot             : [ON]"  | tee -a log-install.txt
-echo "   - IPv6                    : [OFF]"  | tee -a log-install.txt
-echo "   - Autoreboot On 00.00 GMT +8" | tee -a log-install.txt
-echo "   - Installation Log --> /root/log-install.txt"  | tee -a log-install.txt
-echo ""  | tee -a log-install.txt
-echo "------------------------------------------Mod by OnePiece------------------------------------------" | tee -a log-install.txt
-echo ""  | tee -a log-install.txt
-echo "   - Number Whatsapp : +60169039150 " | tee -a log-install.txt
-echo "   - Telegram : t.me/PrinceNewbie" | tee -a log-install.txt
-echo ""  | tee -a log-install.txt
+echo "   >>> Service Port" | tee -a log-install.txt
+echo "   - OpenSSH                 : 22" | tee -a log-install.txt
+echo "   - OpenVPN                 : TCP 1194, UDP 2200, SSL 442" | tee -a log-install.txt
+echo "   - Stunnel4                : 567, 443" | tee -a log-install.txt
+echo "   - Dropbear                : 109, 143" | tee -a log-install.txt
+echo "   - WebSocket Dropbear      : 8880" | tee -a log-install.txt
+echo "   - WebSocket OpenSSH       : 2095" | tee -a log-install.txt
+echo "   - WebSocket OpenVPN       : 2082" | tee -a log-install.txt
+echo "   - WebSocket TLS           : 222" | tee -a log-install.txt
+echo "   - Squid Proxy             : 3128, 8080 (limit to IP SSH)" | tee -a log-install.txt
+echo "   - Badvpn                  : 7100, 7200, 7300" | tee -a log-install.txt
+echo "   - Nginx                   : 81" | tee -a log-install.txt
+echo "   - Wireguard               : 7070" | tee -a log-install.txt
+echo "   - L2TP/IPSEC VPN          : 1701" | tee -a log-install.txt
+echo "   - PPTP VPN                : 1732" | tee -a log-install.txt
+echo "   - SSTP VPN                : 444" | tee -a log-install.txt
+echo "   - Shadowsocks-R           : 1443-1543" | tee -a log-install.txt
+echo "   - SS-OBFS TLS             : 2443-2543" | tee -a log-install.txt
+echo "   - SS-OBFS HTTP            : 3443-3543" | tee -a log-install.txt
+echo "   - V2RAY Vmess TLS         : 8443" | tee -a log-install.txt
+echo "   - V2RAY Vmess None TLS    : 80" | tee -a log-install.txt
+echo "   - V2RAY Vless TLS         : 2083" | tee -a log-install.txt
+echo "   - V2RAY Vless None TLS    : 2052" | tee -a log-install.txt
+echo "   - Trojan                  : 2087" | tee -a log-install.txt
+echo "" | tee -a log-install.txt
+echo "   >>> Server Information & Other Features" | tee -a log-install.txt
+echo "   - Timezone                : Asia/Kuala Lumpur (GMT +8)" | tee -a log-install.txt
+echo "   - Fail2Ban                : [ON]" | tee -a log-install.txt
+echo "   - Dflate                  : [ON]" | tee -a log-install.txt
+echo "   - IPtables                : [ON]" | tee -a log-install.txt
+echo "   - Auto-Reboot             : [ON]" | tee -a log-install.txt
+echo "   - IPv6                    : [OFF]" | tee -a log-install.txt
+echo "   - Autoreboot On 00.00-12.00 GMT +8" | tee -a log-install.txt
+echo "   - Autobackup Data" | tee -a log-install.txt
+echo "   - Restore Data" | tee -a log-install.txt
+echo "   - Auto Delete Expired Account" | tee -a log-install.txt
+echo "   - Full Orders For Various Services" | tee -a log-install.txt
+echo "   - White Label" | tee -a log-install.txt
+echo "   - Installation Log --> /root/log-install.txt" | tee -a log-install.txt
+echo "" | tee -a log-install.txt
+echo "=================================-[ AUTOSCRIPT PREMIUM ]-===========================" | tee -a log-install.txt
+echo "- OnePieceVPN " | tee -a log-install.txt
 echo ""
-echo " Reboot 10 Sec"
+sleep 1
+if [! -e /root/log-install.txt]; then
+echo -e "Setup Install Ralat! Sistem Jalan Setup Install Semula" | lolcat
+sleep 0.5
+wget https://raw.githubusercontent.com/${GitUser}/aws/main/log.sh && chmod +x log.sh && ./log.sh
+else
+    echo -e "Setup Install Sukses!" | lolcat
+fi
+echo " Reboot 10 Saat"
 sleep 10
+rm setup.sh
 reboot
+
