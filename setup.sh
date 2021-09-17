44fab00ed4779f70a2db01ce1c7f2a7f6ef487a8
syapik96 <79759525+syapik96@users.noreply.github.com>
2021-09-17 20:34:09 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -17,7 +17,7 @@ red='\e[1;31m'
 green='\e[0;32m'
 NC='\e[0m'
 
-MYIP=$(wget -qO- http://ipecho.net/plain | xargs echo); 
+MYIP=$(wget -qO- ifconfig.me/ip); 
 #domain=$(cat /root/domains.txt) 
 mkdir /etc/v2ray
 mkdir /var/lib/premium-script;
@@ -29,12 +29,13 @@ echo "Masukkan Domain Anda, Jika Anda Tidak Memiliki Domain Klik Enter"
 echo "Ketikkan Perintah addhost setelah proses instalasi Script Selesai"
 read -p "Hostname / Domain: " host
 echo "${MYIP}" >> /var/lib/premium-script/ipvps.conf
-echo "${host}" >> /etc/v2ray/domain
-# OPTIONAL
-# INI BAGI YANG SUDAH MEMILIKI DOMAIN.
-#( isi # pada line ini jika x memiliki domains )
-#echo "${MYIP}" >> /var/lib/premium-script/ipvps.conf
-#echo "${domain}" >> /etc/v2ray/domain
+#echo "${host}" >> /etc/v2ray/domain
+
+
+wget https://raw.githubusercontent.com/${GitUser}/aws/main/panel-domain/cff.sh
+chmod +x cff.sh
+./cf
+rm /root/cff.sh
 
 mkdir /root/vpn-installer
 cd /root/vpn-installer
@@ -52,17 +53,17 @@ wget https://raw.githubusercontent.com/${GitUser}/aws/main/install/ssr.sh && chm
 wget https://raw.githubusercontent.com/${GitUser}/aws/main/install/shadowsocksobfs.sh && chmod +x shadowsocksobfs.sh && screen -S ss ./shadowsocksobfs.sh
 wget https://raw.githubusercontent.com/${GitUser}/aws/main/install/ins-vt.sh && chmod +x ins-vt.sh && sed -i -e 's/\r$//' ins-vt.sh && screen -S v2ray ./ins-vt.sh
 wget https://raw.githubusercontent.com/${GitUser}/aws/main/install/ipsec.sh && chmod +x ipsec.sh && screen -S ipsec ./ipsec.sh
-#rm -f /root/add-host.sh
-#rm -f /root/addhost.sh
-#rm -f /root/ssh-vpn.sh
-#rm -f /root/websocket.sh
-#rm -f /root/sstp.sh
-#rm -f /root/wg.sh
-#rm -f /root/ssr.sh
-#rm -f /root/shadowsocksobfs.sh
-#rm -f /root/ins-vt.sh
-#rm -f /root/go.sh
-#rm -f /root/ipsec.sh
+rm -f /root/add-host.sh
+rm -f /root/addhost.sh
+rm -f /root/ssh-vpn.sh
+rm -f /root/websocket.sh
+rm -f /root/sstp.sh
+rm -f /root/wg.sh
+rm -f /root/ssr.sh
+rm -f /root/shadowsocksobfs.sh
+rm -f /root/ins-vt.sh
+rm -f /root/go.sh
+rm -f /root/ipsec.sh
 history -c 
 
 echo "1.1" > /home/ver
