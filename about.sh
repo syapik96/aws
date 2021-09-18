f594803329fce36e73a0b62e05e5cf03814df522
syapik96 <79759525+syapik96@users.noreply.github.com>
2021-09-18 15:53:22 +0800

    Update about.sh

--- about.sh
+++ about.sh
@@ -1,19 +1,24 @@
 #!/bin/bash
-
 clear
-echo -e "#====================================================================#"
-echo -e "#                   Premium Script By KASIHENTAMAJE                  #"
-echo -e "#------------------------------------------------------------------- #"
-echo -e "#       ░█▀▀▀█ ░█▄─░█ ░█▀▀▀ 　 ░█▀▀█ ▀█▀ ░█▀▀▀ ░█▀▀█ ░█▀▀▀           #"
-echo -e "#       ░█──░█ ░█░█░█ ░█▀▀▀ 　 ░█▄▄█ ░█─ ░█▀▀▀ ░█─── ░█▀▀▀           #"
-echo -e "#       ░█▄▄▄█ ░█──▀█ ░█▄▄▄ 　 ░█─── ▄█▄ ░█▄▄▄ ░█▄▄█ ░█▄▄▄           #"
-echo -e "#                                                                    #"
-echo -e "#          ☆┌─┐ ─┐☆          - Mod By KASIHENTAMAJE                 #"
-echo -e "#           │▒│ /▒/           - Thanks To Original.Creator           #"
-echo -e "#         　│▒│/▒/            - Whatsapp : +60169039150              #"
-echo -e "#         　│▒ /▒/─┬─┐◯      - Telegram : t.me/PrinceNewbie         #"
-echo -e "#           │▒│▒|▒│▒│         - To who those had use tis script      #"
-echo -e "#          ┌┴─┴─┐-┘─┘           might facing issue,sorry about that. #"
-echo -e "#          │▒┌──┘▒▒▒│◯       - Thanks for who using this script     #"
-echo -e "#          └┐▒▒▒▒▒▒┌┘                                                #"
-echo -e "#====================================================================#"
+figlet "Premium Autoscript Vps" | lolcat
+echo -e "#====================================================================#" | lolcat       
+echo -e "#                   Premium Script By OnePieceVPN                    #"  | lolcat    
+echo -e "#                                                                    #"  | lolcat    
+echo -e "#       ░█▀▀▀█ ░█▄─░█ ░█▀▀▀ 　 ░█▀▀█ ▀█▀ ░█▀▀▀ ░█▀▀█ ░█▀▀▀           #" | lolcat    
+echo -e "#       ░█──░█ ░█░█░█ ░█▀▀▀ 　 ░█▄▄█ ░█─ ░█▀▀▀ ░█─── ░█▀▀▀           #"  | lolcat                  
+echo -e "#       ░█▄▄▄█ ░█──▀█ ░█▄▄▄ 　 ░█─── ▄█▄ ░█▄▄▄ ░█▄▄█ ░█▄▄▄           #"   | lolcat          
+echo -e "#                                                                    #"   | lolcat   
+echo -e "#                           - Modded By PrinceNewbie                 #"  | lolcat 
+echo -e "#           │▒│ /▒/         - Thanks To :                            #"  | lolcat  
+echo -e "#           │▒│/▒/          - Whatsapp  :                           #"  | lolcat     
+echo -e "#         　│▒ /▒/─┬─┐      - Telegram : t.me/PrinceNewbie           #"   | lolcat    
+echo -e "#           │▒│▒|▒│▒│       - To those who use this script,          #"   | lolcat     
+echo -e "#          ┌┴─┴─┐-┘─┘         this script is still under development #" | lolcat
+echo -e "#          │▒┌──┘▒▒▒│        - Thanks for who using this script       #" | lolcat      
+echo -e "#          └┐▒▒▒▒▒▒┌┘       - To EnterMenuList = 'menu'              #"   | lolcat    
+echo -e "#                                                                    #"  | lolcat     
+echo -e "#====================================================================#" | lolcat
+
+echo -e "                         COPYRIGHT © 2021 "    
+echo -e "               ONEPIECEVPN POWERED BY PRINCENEWBIE"
+echo -e "            Privacy Policy Terms & Conditions Disclaimer"  
