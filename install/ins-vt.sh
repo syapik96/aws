5cd969585a504892cc6dbefb3ebfab33a9d5664e
syapik96 <79759525+syapik96@users.noreply.github.com>
2021-09-19 01:15:41 +0800

    Update ins-vt.sh

--- install/ins-vt.sh
+++ install/ins-vt.sh
@@ -8,7 +8,7 @@ apt -y install chrony
 timedatectl set-ntp true
 systemctl enable chronyd && systemctl restart chronyd
 systemctl enable chrony && systemctl restart chrony
-timedatectl set-timezone Asia/Jakarta
+timedatectl set-timezone Asia/Kuala_lumpur
 chronyc sourcestats -v
 chronyc tracking -v
 date
