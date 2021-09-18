0c4b02417fb9722c02c5fb7da5738c181d20ade9
syapik96 <79759525+syapik96@users.noreply.github.com>
2021-09-19 01:16:19 +0800

    Update ins-vt.sh

--- install/ins-vt.sh
+++ install/ins-vt.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-domain=$(cat /root/domain)
+domain=$(cat /etc/hostname)
 apt install iptables iptables-persistent -y
 apt install curl socat xz-utils wget apt-transport-https gnupg gnupg2 gnupg1 dnsutils lsb-release -y 
 apt install socat cron bash-completion ntpdate -y
