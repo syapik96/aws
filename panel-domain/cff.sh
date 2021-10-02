41714d8fb41b3bd2525a557d741938c568c0a8e7
syapik96 <79759525+syapik96@users.noreply.github.com>
2021-10-03 04:39:05 +0800

    Update cff.sh

--- panel-domain/cff.sh
+++ panel-domain/cff.sh
@@ -1,19 +1,18 @@
 #!/bin/bash
 
-cd /root
-rm /root/mail.confconf 
+cd 
+rm /root/mail.conf
 figlet "Cloudflare" | lolcat
-
 echo "Name : Add ID Cloudlare " | lolcat
 echo ""
-read -e -p " Masukan Domain :$domain" domain
+read -e -p " Masukan Domain : " domain
 read -e -p " Masukan Email Cloudflare :" email
 read -e -p " Masukan Api Key :" key
 echo -e "domain=$domain" >> /root/mail.conf
 echo -e "email=$email" >> /root/mail.conf
 echo -e "key=$key" >> /root/mail.conf
 clear
-echo "Done"
+echo -e "Done"
 echo -e "Your ID Cloudflare"
 echo -e "===============================" | lolcat
 echo -e "DOMAIN         : $domain"
