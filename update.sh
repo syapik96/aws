ddac51bf8d3b6f9c4103a2b98475a54e9f5c1094
syapik96 <79759525+syapik96@users.noreply.github.com>
2021-10-03 07:33:42 +0800

    Update update.sh

--- update.sh
+++ update.sh
@@ -6,21 +6,16 @@ fi
 red='\e[1;31m'
 green='\e[0;32m'
 NC='\e[0m'
-MYIP=$(wget -qO- ifconfig.me/ip);
+
 clear
-echo "Starting Update ..." | lolcat
+
 # update
 cd /usr/bin
-rm -f /usr/bin/menu
 GitUser="syapik96"
 #wget https://github.com/${GitUser}/
-wget -O l2tp "https://raw.githubusercontent.com/${GitUser}/aws/main/menu-update/update1.0.sh"
-chmod +x update1.0
-update1.0
+wget -O  update1 "https://raw.githubusercontent.com/${GitUser}/aws/main/menu-update/update1.sh"
+chmod +x update1
+update1
 
-echo " Successfully Update :"
-echo " Successfully Upload **New Menu Section** :"
-echo " - Fix minor Bugs"
-echo " Reboot 5 Sec"
-sleep 5
-echo -e "Please rebbot your server"
+echo -e "Starting Update ..." | lolcat
+
