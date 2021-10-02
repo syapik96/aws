807c72e0f1127ea2e0cd1e2b7a7b26f4ef898fe2
syapik96 <79759525+syapik96@users.noreply.github.com>
2021-10-03 05:06:29 +0800

    Update cff.sh

--- panel-domain/cff.sh
+++ panel-domain/cff.sh
@@ -1,21 +1,35 @@
 #!/bin/bash
+# updated Add cloudflare section
 
 cd 
-rm /root/mail.conf
-figlet "Cloudflare" | lolcat
-echo "Name : Add ID Cloudlare " | lolcat
+rm /root/mail2.txt  /root/mail3.txt  /root/mail4.txt
+figlet -f standard "Cloudflare" | lolcat
 echo ""
-read -e -p " Masukan Domain : " domain
-read -e -p " Masukan Email Cloudflare :" email
-read -e -p " Masukan Api Key :" key
-echo -e "domain=$domain" >> /root/mail.conf
-echo -e "email=$email" >> /root/mail.conf
-echo -e "key=$key" >> /root/mail.conf
+echo -e " ---Add ID Cloudlare--- " | lolcat
+echo ""
+read -p " Masukan Domain : " domain
+read -p " Masukan Email Cloudflare :" email
+read -p " Masukan Api Key :" key
+echo "$domain" >> /root/mail2.txt
+echo "$email" >> /root/mail3.txt
+echo "$key" >> /root/mail4.txt
 clear
-echo -e "Done"
-echo -e "Your ID Cloudflare"
+domain=$(cat /root/mail2.txt)
+email=$(cat /root/mail3.txt)
+key=$(cat /root/mail4.txt)
+figlet -f standard "Succesfully" | lolcat
+echo ""
+echo -e "Your NewID Cloudflare" | lolcat
 echo -e "===============================" | lolcat
-echo -e "DOMAIN         : $domain"
-echo -e "Email          : $email"
-echo -e "Api Key        : $key"
+echo -e " DOMAIN         : $domain"
+echo -e " Email          : $email"
+echo -e " Api Key        : $key"
 echo -e "===============================" | lolcat
+read -p " Enter 0 back menu : " HH
+case $HH in
+    0)
+    clear
+    menu
+    exit
+    ;;
+ esac   
