3a21b99578c942a94c43e03daba06ec86b7fadc9
syapik96 <79759525+syapik96@users.noreply.github.com>
2021-10-03 04:34:27 +0800

    Update add-dns.sh

--- panel-domain/add-dns.sh
+++ panel-domain/add-dns.sh
@@ -1,50 +1,63 @@
 #!/bin/bash
+# Updated October2021
+
+clear
 red='\e[1;31m'
 green='\e[0;32m'
 NC='\e[0m'
 bl='\e[36;1m'
 bd='\e[1m'
 MYIP=$(wget -qO- ifconfig.co);
-echo "Checking VPS"
-clear
-figlet "PANEL DOMAIN" | lolcat
+
+figlet -f standard "PANEL DOMAIN" | lolcat
 echo -e ""
-echo -e " ╠═══════════════════════════════════════╣" | lolcat
-echo -e " ║            Section DOMAIN          ║" | lolcat
-echo -e " ╠═══════════════════════════════════════╣" | lolcat
-echo -e " $bd║\e[m"
-echo -e " $bd║\e[m 1$bl]\e[m$bd Add Subdomain Host For VPS"
-echo -e " $bd║\e[m 2$bl]\e[m$bd Add ID Cloudflare"
-echo -e " $bd║\e[m 3$bl]\e[m$bd Cloudflare Add-Ons" 
-echo -e " $bd║\e[m 4$bl]\e[m$bd Pointing BUG"
-echo -e " $bd║\e[m 5$bl]\e[m$bd Renew Certificate V2RAY"
-echo -e " $bd║\e[m"
-echo -e " ╠═══════════════════════════════════════╣" | lolcat
-echo -e " ║  0]  RETURN  MENU                     ║"   | lolcat
-echo -e " ╠═══════════════════════════════════════╣" | lolcat
+echo -e "  ╠═══════════════════════════════════════╣" | lolcat
+echo -e "  ║            Section DOMAIN             ║" | lolcat
+echo -e "  ╠═══════════════════════════════════════╣" | lolcat
+echo -e " $bd ║"
+echo -e " $bd ║    1 ]  Add Subdomain Host For VPS" | lolcat
+echo -e " $bd ║    2 ]  Add ID Cloudflare" | lolcat
+echo -e " $bd ║    3 ]  Cloudflare Add-Ons" | lolcat
+echo -e " $bd ║    4 ]  Pointing BUG" | lolcat
+echo -e " $bd ║    5 ]  Renew Certificate V2RAY" | lolcat
+echo -e " $bd ║"
+echo -e "  ╠═══════════════════════════════════════╣" | lolcat
+echo -e "  ║  0]  RETURN  MENU                     ║" | lolcat
+echo -e "  ╠═══════════════════════════════════════╣" | lolcat
 echo -e ""
-read -p "     Please select an option  :  "  dns
-echo -e ""
-case $dns in
-1)
-add-host
-;;
-2)
-cff
-;;
-3)
-cfd
-;;
-4)
-cfh
-;;
-5)
-certv2ray
-;;
-0)
-menu
-;;
-*)
-echo "Please enter an correct number"
-;;
-esac
+read -e -p "     Please select an option  :  "  pintu
+case $pintu in
+     1)
+     clear
+     add-host
+     exit
+     ;;
+     2)
+     clear
+     cff
+     exit
+     ;;
+     3)
+     clear
+     cfd
+     exit
+     ;;
+     4)
+     clear
+     cfh
+     exit
+     ;;
+     5)
+     clear
+     certv2ray
+     exit
+     ;;
+     0)
+     clear
+     menu
+     exit
+     ;;
+     *)
+     echo -e "Please enter an correct number"
+     ;;
+ esac
