0263a54c4a61e226078f8ff913b9cd95d408cd98
syapik96 <79759525+syapik96@users.noreply.github.com>
2021-10-20 12:13:06 +0800

    Update and rename addhost.sh to add-host.sh

---
+++ tambah/add-host.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+red='\e[1;31m'
+green='\e[0;32m'
+NC='\e[0m'
+clear
+DOMAIN="$( cat /root/mail1.txt )"
+sub="$(cat /root/mail2.txt )"
+CF_ID="$( cat /root/mail3.txt )"
+CF_KEY="$( cat /root/mail4.txt )"
+SUB_DOMAIN=${sub}.${DOMAIN}
+set -euo pipefail
+IP=$(wget -qO- http://ipecho.net/plain | xargs echo);
+
+echo -e  "\e[0;32mPointing DNS Untuk Domain ${SUB_DOMAIN}...\e[0m"
+
+ZONE=$(curl -sLX GET "https://api.cloudflare.com/client/v4/zones?name=${DOMAIN}&status=active" \
+     -H "X-Auth-Email: ${CF_ID}" \
+     -H "X-Auth-Key: ${CF_KEY}" \
+     -H "Content-Type: application/json" | jq -r .result[0].id)
+
+RECORD=$(curl -sLX GET "https://api.cloudflare.com/client/v4/zones/${ZONE}/dns_records?name=${SUB_DOMAIN}" \
+     -H "X-Auth-Email: ${CF_ID}" \
+     -H "X-Auth-Key: ${CF_KEY}" \
+     -H "Content-Type: application/json" | jq -r .result[0].id)
+
+if [[ "${#RECORD}" -le 10 ]]; then
+     RECORD=$(curl -sLX POST "https://api.cloudflare.com/client/v4/zones/${ZONE}/dns_records" \
+     -H "X-Auth-Email: ${CF_ID}" \
+     -H "X-Auth-Key: ${CF_KEY}" \
+     -H "Content-Type: application/json" \
+     --data '{"type":"A","name":"'${SUB_DOMAIN}'","content":"'${IP}'","ttl":120,"proxied":false}' | jq -r .result.id)
+fi
+
+RESULT=$(curl -sLX PUT "https://api.cloudflare.com/client/v4/zones/${ZONE}/dns_records/${RECORD}" \
+     -H "X-Auth-Email: ${CF_ID}" \
+     -H "X-Auth-Key: ${CF_KEY}" \
+     -H "Content-Type: application/json" \
+     --data '{"type":"A","name":"'${SUB_DOMAIN}'","content":"'${IP}'","ttl":120,"proxied":false}')
+echo "Host : $SUB_DOMAIN"
+echo "IP=$SUB_DOMAIN" >> /var/lib/premium-script/ipvps.conf
