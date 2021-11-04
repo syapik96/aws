bfd24ce5cd9955677aa7f015fefd84b587f223ae
syapik96 <79759525+syapik96@users.noreply.github.com>
2021-11-04 19:46:08 +0800

    Update ssh.sh

--- menu-update/ssh.sh
+++ menu-update/ssh.sh
@@ -22,41 +22,64 @@ echo -e " 7$bl]\e[m$bd  Delete User Expired SSH & OpenVPN"
 echo -e " 8$bl]\e[m$bd  Set up Autokill SSH"
 echo -e " 9$bl]\e[m$bd  Cek Users Who Do Multi Login SSH"
 echo -e "\e[1;32m══════════════════════════════════════════\e[m" | lolcat
-echo -e " x)   MENU        \e[m" | lolcat
+echo -e "    x)   MENU   exit) Just Enter     \e[m" | lolcat
 echo -e "\e[1;32m══════════════════════════════════════════\e[m" | lolcat
-echo -e ""
+echo -e "\e[1;31m"
 read -p "     Please Input Number  [1-9 or x] :  "  ssh
-echo -e ""
+echo -e "\e[0m"
 case $ssh in
 1)
+clear
 usernew
+exit
 ;;
 2)
+clear
 trial
+exit
 ;;
 3)
+clear
 renew
+exit
 ;;
 4)
+clear
 deluser
+exit
 ;;
 5)
+clear
 cek
+exit
 ;;
 6)
+clear
 member
+exit
 ;;
 7)
+clear
 delete
+exit
 ;;
 8)
+clear
 autokill
+exit
 ;;
 9)
+clear
 ceklim
+exit
 ;;
 x)
+clear
+menu
+;;
+clear
 exit
+sudo -i
 ;;
 *)
 echo "Please enter an correct number"
