0662d311c9e77b439251304f9772d8fd66edf14f
syapik96 <79759525+syapik96@users.noreply.github.com>
2021-11-07 20:14:45 +0800

    Update ssh-vpn.sh

--- install/ssh-vpn.sh
+++ install/ssh-vpn.sh
@@ -159,9 +159,9 @@ sed -i "s|IP-ADDRESS|$MYIP|g" /etc/squid/squid.conf
 apt -y install vnstat
 /etc/init.d/vnstat restart
 apt -y install libsqlite3-dev
-wget https://humdi.net/vnstat/vnstat-2.6.tar.gz
-tar zxvf vnstat-2.6.tar.gz
-cd vnstat-2.6
+wget https://humdi.net/vnstat/vnstat-2.8.tar.gz
+tar zxvf vnstat-2.8.tar.gz
+cd vnstat-2.8
 ./configure --prefix=/usr --sysconfdir=/etc && make && make install 
 cd
 vnstat -u -i $NET
@@ -169,8 +169,8 @@ sed -i 's/Interface "'""eth0""'"/Interface "'""$NET""'"/g' /etc/vnstat.conf
 chown vnstat:vnstat /var/lib/vnstat -R
 systemctl enable vnstat
 /etc/init.d/vnstat restart
-rm -f /root/vnstat-2.6.tar.gz 
-rm -rf /root/vnstat-2.6
+rm -f /root/vnstat-2.8.tar.gz 
+rm -rf /root/vnstat-2.8
 
 # install stunnel
 apt -y install stunnel4
