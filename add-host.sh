fec5101adbba704f7f0a321fc55dfa33bed197a1
syapik96 <79759525+syapik96@users.noreply.github.com>
2021-11-18 23:46:10 +0800

    Update add-host.sh

--- add-host.sh
+++ add-host.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 ### 🔰 COPYRIGHT © 2021 OnePieceVPN, Inc 🔰 ###
 ### Script Update by : t.me/PrinceNewbie ####
+
+echo -e "\e[1;31m PASTIKAN DOMAIN SUDAH POINTING IP VPS ANDA \e[0m"
+echo -e "\e[1;31mJika Tiada domain sila Create Domain dahulu" 
+echo -e "Contonya di Freenom(free) \e[0m" 
+
 read -rp "Domain/Host: " -e host
 echo "IP=$host">>/var/lib/premium-script/ipvps.conf
 
