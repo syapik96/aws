6a00b342e3238af0906e542ac63661a154491075
syapik96 <79759525+syapik96@users.noreply.github.com>
2021-11-18 22:51:35 +0800

    Update cf.sh

--- cf.sh
+++ cf.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
-# Update By Prince NewBie
-# Telegram: https://t.me/PrinceNewbie
-
-clear
-
-DOMAIN=$( cat /root/mail1.txt )
-sub=$(cat /root/mail2.txt )
-CF_ID=$( cat /root/mail3.txt )
-CF_KEY=$( cat /root/mail4.txt )
-SUB_DOMAIN=${sub}.${DOMAIN}
-
+red='\e[1;31m'
+green='\e[0;32m'
+NC='\e[0m'
+MYIP=$(wget -qO- http://ipecho.net/plain);
+echo "Checking VPS"
+apt install jq curl -y
+DOMAIN=gilergames.tk
+sub=$(</dev/urandom tr -dc a-z0-9 | head -c4)
+SUB_DOMAIN=${sub}.gilergames.tk
+CF_ID=zulhisyam421@gmail.com
+CF_KEY=48e94b491ba3933abe8732b9a5f91cf3f3c36
 set -euo pipefail
-IP=$(wget -qO- ifconfig.co);
-echo "Updating DNS for ${SUB_DOMAIN}..." | lolcat 
+IP=$(wget -qO- http://ipecho.net/plain);
+echo "Updating DNS for ${SUB_DOMAIN}..."
 ZONE=$(curl -sLX GET "https://api.cloudflare.com/client/v4/zones?name=${DOMAIN}&status=active" \
      -H "X-Auth-Email: ${CF_ID}" \
      -H "X-Auth-Key: ${CF_KEY}" \
@@ -38,3 +38,4 @@ RESULT=$(curl -sLX PUT "https://api.cloudflare.com/client/v4/zones/${ZONE}/dns_r
      --data '{"type":"A","name":"'${SUB_DOMAIN}'","content":"'${IP}'","ttl":120,"proxied":false}')
 echo "Host : $SUB_DOMAIN"
 echo $SUB_DOMAIN > /root/domain
+rm -f /root/cf.sh
