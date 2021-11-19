d795b0a8c172afb162ddfe76bacf9f817a030ef5
syapik96 <79759525+syapik96@users.noreply.github.com>
2021-11-19 17:11:58 +0800

    Update and rename ws-ovpn to ws-ovpn.py

--- websocket-python/ws-ovpn.py
+++ websocket-python/ws-ovpn.py
@@ -13,7 +13,7 @@ PASS = ''
 BUFLEN = 4096 * 4
 TIMEOUT = 60
 DEFAULT_HOST = '127.0.0.1:1194'
-RESPONSE = 'HTTP/1.1 101 OpenVPN WebSocket By @PrinceNewbie\r\n\r\n'
+RESPONSE = 'HTTP/1.1 101 OpenVPN_WebSocket_By_PrinceNewbie\r\n\r\n'
 #RESPONSE = 'HTTP/1.1 200 Hello_World!\r\nContent-length: 0\r\n\r\nHTTP/1.1 200 Connection established\r\n\r\n'  # lint:ok
 
 class Server(threading.Thread):
