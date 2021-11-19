9e07ee13c8d07beb4f3963f4356bcdee7385d35e
syapik96 <79759525+syapik96@users.noreply.github.com>
2021-11-19 12:36:24 +0800

    Update add-host.sh

--- add-host.sh
+++ add-host.sh
@@ -6,6 +6,7 @@ echo -e "\e[1;31m PASTIKAN DOMAIN SUDAH POINTING IP VPS ANDA \e[0m"
 echo -e "\e[1;31mJika Tiada domain sila Create Domain dahulu" 
 echo -e "Contonya di Freenom(free) \e[0m" 
 
-read -rp "Domain/Host: " -e host
+read -rp "Domain / Host : " -e host
 echo "IP=$host" > /var/lib/premium-script/ipvps.conf
-
+echo "$host" > /etc/v2ray/domain
+echo "$host" > /root/domain
