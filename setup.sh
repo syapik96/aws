08e5c8a7239a0073ec513501d93bb022d9737eda
syapik96 <79759525+syapik96@users.noreply.github.com>
2021-11-23 06:29:53 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -51,21 +51,21 @@ clear
 echo " "
 echo "Installation has been completed!!"
 echo " "
-echo "===========================-Script Mod by InternetVPN-Project-=========================" | tee -a log-install.txt
+echo "===========================-AutoScript by PrinceNewbie-=========================" | tee -a log-install.txt
 echo "" | tee -a log-install.txt
 echo "----------------------------------------------------------------------------------" | tee -a log-install.txt
 echo ""  | tee -a log-install.txt
 echo "   >>> Service & Port"  | tee -a log-install.txt
-echo "   - OpenSSH                  : 22, 500, X40000"  | tee -a log-install.txt
+echo "   - OpenSSH                  : 22, 226, X40000"  | tee -a log-install.txt
 echo "   - SSH-WS Python OpenSSH    : 100"  | tee -a log-install.txt
 echo "   - SSH-WS Python Dropbear   : 2020, 2021"  | tee -a log-install.txt
 echo "   - SSH-WS Python SSL/TLS    : 5052, 5053"  | tee -a log-install.txt
-echo "   - OpenVPN-WS Python        : 6969"  | tee -a log-install.txt
-echo "   - OpenVPN                  : TCP 1194, UDP 2200, SSL 992, X1197"  | tee -a log-install.txt
-echo "   - Stunnel4 SSL/TLS         : 443, X30000"  | tee -a log-install.txt
-echo "   - Dropbear                 : 143, 109, X50000"  | tee -a log-install.txt
-echo "   - Squid Proxy              : 3128, 8080 (limit to IP Server)"  | tee -a log-install.txt
-echo "   - Badvpn                   : 7100, 7200, 7300"  | tee -a log-install.txt
+echo "   - OpenVPN-WS Python        : 441"  | tee -a log-install.txt
+echo "   - OpenVPN                  : TCP 1194, UDP 2200, SSL 442, X1197"  | tee -a log-install.txt
+echo "   - Stunnel4 SSL/TLS         : 443, 444, X30000"  | tee -a log-install.txt
+echo "   - Dropbear                 : 143, 109, 110, 80(test) X50000"  | tee -a log-install.txt
+echo "   - Squid Proxy              : 3128, 8080, 8000 (limit to IP Server)"  | tee -a log-install.txt
+echo "   - Badvpn                   : 7100, 7200, 7300, 7400, 7500"  | tee -a log-install.txt
 echo "   - Nginx                    : 81"  | tee -a log-install.txt
 echo "   - Wireguard                : 7070"  | tee -a log-install.txt
 echo "   - L2TP/IPSEC VPN           : 1701"  | tee -a log-install.txt
@@ -74,18 +74,20 @@ echo "   - SSTP VPN                 : 5555"  | tee -a log-install.txt
 echo "   - Shadowsocks-R            : 1443-1543"  | tee -a log-install.txt
 echo "   - SS-OBFS TLS              : 2443-2543"  | tee -a log-install.txt
 echo "   - SS-OBFS HTTP             : 3443-3453"  | tee -a log-install.txt
-echo "   - V2RAY Vmess TLS          : 4443"  | tee -a log-install.txt
-echo "   - V2RAY Vmess None TLS     : 80"  | tee -a log-install.txt
-echo "   - Trojan                   : 6443"  | tee -a log-install.txt
+echo "   - V2RAY Vmess TLS          : 8443"  | tee -a log-install.txt
+echo "   - V2RAY Vmess None TLS     : 8880"  | tee -a log-install.txt
+echo "   - V2RAY Vless TLS          : 2083"  | tee -a log-install.txt
+echo "   - V2RAY Vless None TLS     : 2052"  | tee -a log-install.txt
+echo "   - Trojan                   : 2087"  | tee -a log-install.txt
 echo ""  | tee -a log-install.txt
 echo "   >>> Server Information & Other Features"  | tee -a log-install.txt
-echo "   - Timezone                : Asia/Jakarta (GMT +7)"  | tee -a log-install.txt
+echo "   - Timezone                : Asia/Singapore (GMT +8)"  | tee -a log-install.txt
 echo "   - Fail2Ban                : [ON]"  | tee -a log-install.txt
 echo "   - Dflate                  : [ON]"  | tee -a log-install.txt
 echo "   - IPtables                : [ON]"  | tee -a log-install.txt
 echo "   - Auto-Reboot             : [ON]"  | tee -a log-install.txt
 echo "   - IPv6                    : [OFF]"  | tee -a log-install.txt
-echo "   - Autoreboot On 00.00 GMT +7" | tee -a log-install.txt
+echo "   - Autoreboot On 00.00 GMT +8" | tee -a log-install.txt
 echo "   - Installation Log --> /root/log-install.txt"  | tee -a log-install.txt
 echo ""  | tee -a log-install.txt
 echo "------------------------------------------Mod by WHY------------------------------------------" | tee -a log-install.txt
@@ -94,5 +96,8 @@ echo " Username Telegram : @PrinceNewbie" | tee -a log-install.txt
 echo "" | tee -a log-install.txt
 echo ""
 echo " Reboot 10 Sec"
+# port 80 test setup ssh & openvpn websocket 
+
+sed -i 's/DROPBEAR_EXTRA_ARGS="-p 50000 -p 109 -p 110"/DROPBEAR_EXTRA_ARGS="-p 50000 -p 109 -p 110 -p 80"/g' /etc/default/dropbear
 sleep 10
 reboot
