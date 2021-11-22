a814bd77c886a3a872e4ce03ec221d559d0ad0eb
syapik96 <79759525+syapik96@users.noreply.github.com>
2021-11-22 15:03:39 +0800

    Update ssh.sh

--- menu-update/ssh.sh
+++ menu-update/ssh.sh
@@ -1,32 +1,29 @@
 #!/bin/bash
 # updated by syapik96
 
-red='\e[1;31m'
-green='\e[0;32m'
-NC='\e[0m'
-bl='\e[36;1m'
-bd='\e[1m'
-
 clear
 figlet -f slant "PANEL SSH-OpenVPN" | lolcat
 echo -e ""
-echo -e "═════════════════════════════════════════" | lolcat
-echo -e "          \e[1;31m\e[1;31m═[\e[mSSH & OpenVPN Menu\e[1;31m]═\e[m" 
-echo -e "═════════════════════════════════════════" | lolcat
-echo -e " 1$bl]\e[m$bd  Create SSH & OpenVPN Account"
-echo -e " 2$bl]\e[m$bd  Trial Account SSH & OpenVPN"
-echo -e " 3$bl]\e[m$bd  Renew SSH & OpenVPN Account"
-echo -e " 4$bl]\e[m$bd  Delete SSH & OpenVPN Account"
-echo -e " 5$bl]\e[m$bd  Check User Login SSH & OpenVPN"
-echo -e " 6$bl]\e[m$bd  List Member SSH & OpenVPN"
-echo -e " 7$bl]\e[m$bd  Delete User Expired SSH & OpenVPN"
-echo -e " 8$bl]\e[m$bd  Set up Autokill SSH"
-echo -e " 9$bl]\e[m$bd  Cek Users Who Do Multi Login SSH"
-echo -e "══════════════════════════════════════════" | lolcat
-echo -e "    x)   MENU   exit) Just Enter     " | lolcat
-echo -e "══════════════════════════════════════════" | lolcat
+echo -e  "  ╔════════════════════════════════════════════════════════════╗" | lolcat
+echo -e  "  ║                     SSH & OpenVPN Menu                     ║" | lolcat 
+echo -e  "  ╠════════════════════════════════════════════════════════════╝" | lolcat
+echo -e  " $bl ║"| lolcat
+echo -e  " $bl ║\e[m$bd 1$bl]\e[m$bd Create SSH & OpenVPN Account"
+echo -e  " $bl ║\e[m$bd 2$bl]\e[m$bd Trial Account SSH & OpenVPN"
+echo -e  " $bl ║\e[m$bd 3$bl]\e[m$bd Renew SSH & OpenVPN Account"
+echo -e  " $bl ║\e[m$bd 4$bl]\e[m$bd Delete SSH & OpenVPN Account"
+echo -e  " $bl ║\e[m$bd 5$bl]\e[m$bd Check User Login SSH & OpenVPN"
+echo -e  " $bl ║\e[m$bd 6$bl]\e[m$bd List Member SSH & OpenVPN"
+echo -e  " $bl ║\e[m$bd 7$bl]\e[m$bd Delete User Expired SSH & OpenVPN"
+echo -e  " $bl ║\e[m$bd 8$bl]\e[m$bd Set up Autokill SSH"
+echo -e  " $bl ║\e[m$bd 9$bl]\e[m$bd Cek Users Who Do Multi Login SSH"
+echo -e  " $bl ║"| lolcat
+echo -e  "  ╠════════════════════════════════════════════════════════════╣" | lolcat
+echo -e  "  ║            0 ) Main Menu      x ) Exit                     ║" | lolcat
+echo -e  "  ║      COPYRIGHT ONEPIECEVPN, POWERED BY PRINCENEWBIE.       ║" | lolcat
+echo -e  "  ╚════════════════════════════════════════════════════════════╝" | lolcat
 echo -e "\e[1;31m"
-read -p "     Please Input Number  [1-9 or x] :  " ssh
+read -p "            Please Input Number :  " ssh
 echo -e "\e[0m"
 case $ssh in
 	1)
@@ -83,5 +80,6 @@ case $ssh in
 	clear
 	sudo -i
 	neofetch
+	exit
 	;;
 esac
