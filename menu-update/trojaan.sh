76030a1433388bd3bf3f191d8b6ac9283a5c2847
syapik96 <79759525+syapik96@users.noreply.github.com>
2021-11-22 20:46:57 +0800

    Update trojaan.sh

--- menu-update/trojaan.sh
+++ menu-update/trojaan.sh
@@ -1,26 +1,21 @@
 #!/bin/bash
 
-clear
-red='\e[1;31m'
-green='\e[0;32m'
-NC='\e[0m'
-bl='\e[36;1m'
-bd='\e[1m'
-
 figlet -f slant "PANEL TROJAN" | lolcat
 echo -e ""
-echo -e " ╠═══════════════════════════════════════╣" | lolcat
-echo -e " ║           Section  TROJAN             ║" | lolcat
-echo -e " ╠═══════════════════════════════════════╣" | lolcat
-echo -e " $bd║\e[0m"
-echo -e " ║ 1$bl]\e[m$bd Create Trojan Account"
-echo -e " ║ 2$bl]\e[m$bd Deleting Trojan Account"
-echo -e " ║ 3$bl]\e[m$bd Renew Trojan Account"
-echo -e " ║ 4$bl]\e[m$bd Check User Login Trojan"
-echo -e " $bd║\e[0m"
-echo -e " ╠═══════════════════════════════════════╣" | lolcat
-echo -e " ║  0]  RETURN  MENU                     ║"   | lolcat
-echo -e " ╠═══════════════════════════════════════╣" | lolcat
+echo -e  "  ╔═══════════════════════════════════════╗" | lolcat
+echo -e  "  ║               TROJAN                  ║" | lolcat
+echo -e  "  ╠═══════════════════════════════════════╝" | lolcat
+echo -e  "  ║" | lolcat
+echo -e  "  ║ 1$bl]\e[m$bd Create Trojan Account"
+echo -e  "  ║ 2$bl]\e[m$bd Deleting Trojan Account"
+echo -e  "  ║ 3$bl]\e[m$bd Renew Trojan Account"
+echo -e  "  ║ 4$bl]\e[m$bd Check User Login Trojan"
+echo -e  "  ║" | lolcat
+echo -e  "  ║     0 ] Menu      x ] Exit" |  lolcat              
+echo -e  "  ║" | lolcat
+echo -e  "  ╠═══════════════════════════════════════╗" | lolcat
+echo -e  "  ║    COPYRIGHT 2021 OnePieceVPN Inc     ║" | lolcat
+echo -e  "  ╚═══════════════════════════════════════╝" | lolcat
 echo -e "\e[1;32m"
 read -p "     Please select an option :  "  trojan
 echo -e "\e[0m"
