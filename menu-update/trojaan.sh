5d0f2786471e03332e3ad830f0e3bfa8229163e5
syapik96 <79759525+syapik96@users.noreply.github.com>
2021-11-22 14:43:11 +0800

    Update trojaan.sh

--- menu-update/trojaan.sh
+++ menu-update/trojaan.sh
@@ -1,43 +1,55 @@
 #!/bin/bash
+
+clear
 red='\e[1;31m'
 green='\e[0;32m'
 NC='\e[0m'
 bl='\e[36;1m'
 bd='\e[1m'
-MYIP=$(wget -qO- ifconfig.co);
-echo "Checking VPS"
-clear
+
+figlet -f slant "PANEL TROJAN" | lolcat
 echo -e ""
-figlet "PANEL TROJAN" | lolcat
 echo -e " ╠═══════════════════════════════════════╣" | lolcat
 echo -e " ║           Section  TROJAN             ║" | lolcat
 echo -e " ╠═══════════════════════════════════════╣" | lolcat
-echo -e " $bd║\e[m"
-echo -e " $bd║\e[m 1$bl]\e[m$bd Create Trojan Account"
-echo -e " $bd║\e[m 2$bl]\e[m$bd Deleting Trojan Account"
-echo -e " $bd║\e[m 3$bl]\e[m$bd Renew Trojan Account"
-echo -e " $bd║\e[m 4$bl]\e[m$bd Check User Login Trojan"
-echo -e " $bd║\e[m"
+echo -e " $bd║\e[0m"
+echo -e " ║ 1$bl]\e[m$bd Create Trojan Account"
+echo -e " ║ 2$bl]\e[m$bd Deleting Trojan Account"
+echo -e " ║ 3$bl]\e[m$bd Renew Trojan Account"
+echo -e " ║ 4$bl]\e[m$bd Check User Login Trojan"
+echo -e " $bd║\e[0m"
 echo -e " ╠═══════════════════════════════════════╣" | lolcat
 echo -e " ║  0]  RETURN  MENU                     ║"   | lolcat
 echo -e " ╠═══════════════════════════════════════╣" | lolcat
-echo -e ""
+echo -e "\e[1;32m"
 read -p "     Please select an option :  "  trojan
-echo -e ""
+echo -e "\e[0m"
 case $trojan in
-1)
-add-tr
-;;
-2)
-del-tr
-;;
-3)
-renew-tr
-;;
-0)
-menu
-;;
-*)
-echo "Please enter an correct number"
-;;
-esac
+      1)
+      clear
+      addtr
+      exit
+      ;;
+      2)
+      clear
+      deltr
+      exit
+      ;;
+      3)
+      clear
+      renewtr
+      exit
+      ;;
+      0)
+      clear
+      menu
+      exit
+      ;;
+      *)
+      echo "${red}Please enter an correct number${NC}"
+      sleep 3
+      clear
+      trojaan
+      exit
+      ;;
+  esac
