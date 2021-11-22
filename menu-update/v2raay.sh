d2022691ab326ea0c687c779a067f2bdc96bd5ba
syapik96 <79759525+syapik96@users.noreply.github.com>
2021-11-22 21:20:03 +0800

    Update v2raay.sh

--- menu-update/v2raay.sh
+++ menu-update/v2raay.sh
@@ -1,45 +1,65 @@
 #!/bin/bash
+
 red='\e[1;31m'
 green='\e[0;32m'
 NC='\e[0m'
 bl='\e[36;1m'
 bd='\e[1m'
-MYIP=$(wget -qO- ifconfig.co);
-echo "Checking VPS"
+
 clear
 figlet "Panel V2RAY" | lolcat
-echo -e " ╠═══════════════════════════════════════╣" | lolcat
-echo -e " ║            Section  V2RAY               ║" | lolcat
-echo -e " ╠═══════════════════════════════════════╣" | lolcat
+echo ""
+echo -e " ╔═══════════════════════════════════════╗" | lolcat
+echo -e " ║            Section  V2RAY             ║" | lolcat
+echo -e " ╠═══════════════════════════════════════╝" | lolcat
 echo -e " $bd║\e[m"
 echo -e " $bd║\e[m 1$bl]\e[m$bd Create Vmess Websocket Account"
 echo -e " $bd║\e[m 2$bl]\e[m$bd Delete Vmess Websocket Account"
 echo -e " $bd║\e[m 3$bl]\e[m$bd Renew Vmess Account"
 echo -e " $bd║\e[m 4$bl]\e[m$bd Check User Login Vmess"
 echo -e " $bd║\e[m"
-echo -e " ╠═══════════════════════════════════════╣" | lolcat
-echo -e " ║  0]  RETURN  MENU                     ║"   | lolcat
-echo -e " ╠═══════════════════════════════════════╣" | lolcat
-echo -e ""
-read -p "     Please select an option :  "  v2ray
-echo -e ""
+echo -e " $bd║\e[m   0 $bl]\e[m$bd MENU    x $bl]\e[m$bd Exit"
+echo -e " $bd║\e[m"
+echo -e " ╠═══════════════════════════════════════╗" | lolcat
+echo -e " ║  0]  RETURN  MENU                     ║" | lolcat
+echo -e " ╚═══════════════════════════════════════╝" | lolcat
+echo -e "\e[1;31m"
+read -p "    Please select an option :  "  v2ray
+echo -e "\e[0m"
 case $v2ray in
-1)
-add-ws
-;;
-2)
-del-ws
-;;
-3)
-renew-ws
-;;
-4)
-cek-ws
-;;
-0)
-menu
-;;
-*)
-echo "Please enter an correct number"
-;;
+  1)
+  clear
+  addws
+  exit
+  ;;
+  2)
+  clear
+  delws
+  exit
+  ;;
+  3)
+  clear
+  renewws
+  exit
+  ;;
+  4)
+  clear
+  cekws
+  exit
+  ;;
+  0)
+  clear
+  menu
+  exit
+  ;;
+  x)
+  sudo -i
+  exit
+  *)
+  echo -e "${red}Please enter an correct number${NC}"
+  sleep 2
+  clear
+  v2raay
+  exit
+  ;;
 esac
