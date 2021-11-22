a8c6729401e47ca9f15adc7337637a5726aa4ee1
syapik96 <79759525+syapik96@users.noreply.github.com>
2021-11-23 01:22:01 +0800

    Update ws-stunnel.py

--- websocket-python/ws-stunnel.py
+++ websocket-python/ws-stunnel.py
@@ -14,7 +14,7 @@ PASS = ''
 BUFLEN = 4096 * 4
 TIMEOUT = 60
 DEFAULT_HOST = '127.0.0.1:143'
-RESPONSE = 'HTTP/1.1 101 PrinceNewbie\r\nContent-Length: 1048576000000\r\n\r\n'
+RESPONSE = 'HTTP/1.1 101 Switching Protocols\r\nContent-Length: 1048576000000\r\n\r\n'
 #RESPONSE = 'HTTP/1.1 200 Hello_World!\r\nContent-length: 0\r\n\r\nHTTP/1.1 200 Connection established\r\n\r\n'  # lint:ok
 
 class Server(threading.Thread):
