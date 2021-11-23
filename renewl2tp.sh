503cd80221f5410498f36ea538c3828500df38ef
syapik96 <79759525+syapik96@users.noreply.github.com>
2021-11-23 14:31:12 +0800

    Update renewl2tp.sh

--- renewl2tp.sh
+++ renewl2tp.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-NUMBER_OF_CLIENTS=$(grep -c -E "^### " "/var/lib/crot-script/data-user-l2tp")
+NUMBER_OF_CLIENTS=$(grep -c -E "^### " "/var/lib/premium-script/data-user-l2tp")
 	if [[ ${NUMBER_OF_CLIENTS} == '0' ]]; then
 		clear
 		echo ""
@@ -12,7 +12,7 @@ NUMBER_OF_CLIENTS=$(grep -c -E "^### " "/var/lib/crot-script/data-user-l2tp")
 	echo "Select the existing client you want to renew"
 	echo " Press CTRL+C to return"
 	echo -e "==============================="
-	grep -E "^### " "/var/lib/crot-script/data-user-l2tp" | cut -d ' ' -f 2-3 | nl -s ') '
+	grep -E "^### " "/var/lib/premium-script/data-user-l2tp" | cut -d ' ' -f 2-3 | nl -s ') '
 	until [[ ${CLIENT_NUMBER} -ge 1 && ${CLIENT_NUMBER} -le ${NUMBER_OF_CLIENTS} ]]; do
 		if [[ ${CLIENT_NUMBER} == '1' ]]; then
 			read -rp "Select one client [1]: " CLIENT_NUMBER
@@ -21,15 +21,15 @@ NUMBER_OF_CLIENTS=$(grep -c -E "^### " "/var/lib/crot-script/data-user-l2tp")
 		fi
 	done
 read -p "Expired (days): " masaaktif
-user=$(grep -E "^### " "/var/lib/crot-script/data-user-l2tp" | cut -d ' ' -f 2 | sed -n "${CLIENT_NUMBER}"p)
-exp=$(grep -E "^### " "/var/lib/crot-script/data-user-l2tp" | cut -d ' ' -f 3 | sed -n "${CLIENT_NUMBER}"p)
+user=$(grep -E "^### " "/var/lib/premium-script/data-user-l2tp" | cut -d ' ' -f 2 | sed -n "${CLIENT_NUMBER}"p)
+exp=$(grep -E "^### " "/var/lib/premium-script/data-user-l2tp" | cut -d ' ' -f 3 | sed -n "${CLIENT_NUMBER}"p)
 now=$(date +%Y-%m-%d)
 d1=$(date -d "$exp" +%s)
 d2=$(date -d "$now" +%s)
 exp2=$(( (d1 - d2) / 86400 ))
 exp3=$(($exp2 + $masaaktif))
 exp4=`date -d "$exp3 days" +"%Y-%m-%d"`
-sed -i "s/### $user $exp/### $user $exp4/g" /var/lib/crot-script/data-user-l2tp
+sed -i "s/### $user $exp/### $user $exp4/g" /var/lib/premium-script/data-user-l2tp
 clear
 echo ""
 echo " Akun L2TP berhasil diperpanjang"
