d6fa26a608542e72a358738ecff5c30b034ce621
syapik96 <79759525+syapik96@users.noreply.github.com>
2021-11-23 19:06:58 +0800

    Update ssh-vpn.sh

--- install/ssh-vpn.sh
+++ install/ssh-vpn.sh
@@ -155,7 +155,7 @@ sed -i 's/Port 40000/g' /etc/ssh/sshd_config
 apt-get -y install dropbear
 sed -i 's/NO_START=1/NO_START=0/g' /etc/default/dropbear
 sed -i 's/DROPBEAR_PORT=22/DROPBEAR_PORT=143/g' /etc/default/dropbear
-sed -i 's/DROPBEAR_EXTRA_ARGS=/DROPBEAR_EXTRA_ARGS="-p 50000 -p 109 -p 110"/g' /etc/default/dropbear
+sed -i 's/DROPBEAR_EXTRA_ARGS=/DROPBEAR_EXTRA_ARGS="-p 50000 -p 109"/g' /etc/default/dropbear
 echo "/bin/false" >> /etc/shells
 echo "/usr/sbin/nologin" >> /etc/shells
 /etc/init.d/dropbear restart
@@ -215,14 +215,6 @@ connect = 127.0.0.1:143
 [openvpn]
 accept = 992
 connect = 127.0.0.1:1194
-
-[sslopenvpn]
-accept = 442
-connect = 127.0.0.1:1194
-
-[openvpn]
-accept = 110
-connect = 127.0.0.1:1197
 
 END
 
