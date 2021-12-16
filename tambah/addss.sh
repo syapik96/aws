c73ec2982810fd2083d4a25d39e68901009ffab2
syapik96 <79759525+syapik96@users.noreply.github.com>
2021-12-16 20:26:12 +0800

    Update addss.sh

--- tambah/addss.sh
+++ tambah/addss.sh
@@ -14,7 +14,6 @@ http="$((lastport2+1))"
 fi
 echo ""
 echo "Masukkan password"
-
 until [[ $user =~ ^[a-zA-Z0-9_]+$ && ${CLIENT_EXISTS} == '0' ]]; do
 		read -rp "Password: " -e user
 		CLIENT_EXISTS=$(grep -w $user /etc/shadowsocks-libev/akun.conf | wc -l)
@@ -29,7 +28,8 @@ read -p "Expired (hari): " masaaktif
 exp=`date -d "$masaaktif days" +"%Y-%m-%d"`
 tgl=$(echo "$exp" | cut -d- -f3)
 bln=$(echo "$exp" | cut -d- -f2)
-cat > /etc/shadowsocks-libev/$user-tls.json<<END
+
+cat > /etc/shadowsocks-libev/$user-tls.json <<-END
 {   
     "server":"0.0.0.0",
     "server_port":$tls,
@@ -44,6 +44,7 @@ cat > /etc/shadowsocks-libev/$user-tls.json<<END
     "plugin_opts":"obfs=tls"
 }
 END
+
 cat > /etc/shadowsocks-libev/$user-http.json <<-END
 {
     "server":"0.0.0.0",
@@ -59,6 +60,7 @@ cat > /etc/shadowsocks-libev/$user-http.json <<-END
     "plugin_opts":"obfs=http"
 }
 END
+
 chmod +x /etc/shadowsocks-libev/$user-tls.json
 chmod +x /etc/shadowsocks-libev/$user-http.json
 
@@ -67,26 +69,27 @@ systemctl enable shadowsocks-libev-server@$user-tls.service
 systemctl start shadowsocks-libev-server@$user-http.service
 systemctl enable shadowsocks-libev-server@$user-http.service
 
-echo -e "### $user $exp
-port_tls $tls
-port_http $http">>"/etc/shadowsocks-libev/akun.conf"
+echo -e "### $user $exp 
+port_tls $tls 
+port_http $http" >> "/etc/shadowsocks-libev/akun.conf"
 tmp1=$(echo -n "aes-256-cfb:${user}@${IP}:$tls" | base64 -w0)
 tmp2=$(echo -n "aes-256-cfb:${user}@${IP}:$http" | base64 -w0)
-linkss1="ss://${tmp1}?plugin=obfs-local;obfs=tls;obfs-host=google.com"
-linkss2="ss://${tmp2}?plugin=obfs-local;obfs=http;obfs-host=google.com"
+linkss1="ss://${tmp1}?plugin=obfs-local;obfs=tls;obfs-host=your.bug.com"
+linkss2="ss://${tmp2}?plugin=obfs-local;obfs=http;obfs-host=your.bug.com"
 clear
-	echo -e ""
-	echo -e "=======-Shadowsocks-======="
-	echo -e "IP/Host        : $IP"
-	echo -e "Host           : $domain"
-	echo -e "Port OBFS TLS  : $tls"
-	echo -e "Port OBFS HTTP : $http"
-	echo -e "Password       : $user"
-	echo -e "Method         : aes-256-cfb"
-	echo -e "Aktif Sampai   : $exp"
-	echo -e "==========================="
-	echo -e "Link OBFS TLS : $linkss1"
-	echo -e "==========================="
-	echo -e "Link OBFS HTTP : $linkss2"
-	echo -e "==========================="
-	echo -e "Script Mod by OnePiece"
+
+echo -e ""
+echo -e "=======-Shadowsocks-======="
+echo -e "IP/Host        : $IP"
+echo -e "Host           : $domain"
+echo -e "Port OBFS TLS  : $tls"
+echo -e "Port OBFS HTTP : $http"
+echo -e "Password       : $user"
+echo -e "Method         : aes-256-cfb"
+echo -e "Aktif Sampai   : $exp"
+echo -e "==========================="
+echo -e "Link OBFS TLS : $linkss1"
+echo -e "==========================="
+echo -e "Link OBFS HTTP : $linkss2"
+echo -e "==========================="
+echo -e "Script Mod by OnePiece"
