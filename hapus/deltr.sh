014643eb2001b0e70bd1dd2dbf7b5f9248dd883e
syapik96 <79759525+syapik96@users.noreply.github.com>
2021-12-16 22:49:10 +0800

    Update deltr.sh

--- hapus/deltr.sh
+++ hapus/deltr.sh
@@ -1,18 +1,21 @@
 #!/bin/bash
-NUMBER_OF_CLIENTS=$(grep -c -E "^### " "/etc/v2ray/trojan.json")
+red='\e[1;31m'
+green='\e[0;32m'
+NC='\e[0m'
+clear
+NUMBER_OF_CLIENTS=$(grep -c -E "^### " "/etc/trojan/akun.conf")
 	if [[ ${NUMBER_OF_CLIENTS} == '0' ]]; then
 		echo ""
 		echo "You have no existing clients!"
 		exit 1
 	fi
 
-	clear
 	echo ""
 	echo " Select the existing client you want to remove"
 	echo " Press CTRL+C to return"
 	echo " ==============================="
 	echo "     No  Expired   User"
-	grep -E "^### " "/etc/v2ray/trojan.json" | cut -d ' ' -f 2-3 | nl -s ') '
+	grep -E "^### " "/etc/trojan/akun.conf" | cut -d ' ' -f 2-3 | nl -s ') '
 	until [[ ${CLIENT_NUMBER} -ge 1 && ${CLIENT_NUMBER} -le ${NUMBER_OF_CLIENTS} ]]; do
 		if [[ ${CLIENT_NUMBER} == '1' ]]; then
 			read -rp "Select one client [1]: " CLIENT_NUMBER
@@ -20,14 +23,17 @@ NUMBER_OF_CLIENTS=$(grep -c -E "^### " "/etc/v2ray/trojan.json")
 			read -rp "Select one client [1-${NUMBER_OF_CLIENTS}]: " CLIENT_NUMBER
 		fi
 	done
-user=$(grep -E "^### " "/etc/v2ray/trojan.json" | cut -d ' ' -f 2 | sed -n "${CLIENT_NUMBER}"p)
-exp=$(grep -E "^### " "/etc/v2ray/trojan.json" | cut -d ' ' -f 3 | sed -n "${CLIENT_NUMBER}"p)
-sed -i "/^### $user $exp/,/^},{/d" /etc/v2ray/trojan.json
-systemctl restart v2ray@trojan
+CLIENT_NAME=$(grep -E "^### " "/etc/trojan/akun.conf" | cut -d ' ' -f 2-3 | sed -n "${CLIENT_NUMBER}"p)
+user=$(grep -E "^### " "/etc/trojan/akun.conf" | cut -d ' ' -f 2 | sed -n "${CLIENT_NUMBER}"p)
+exp=$(grep -E "^### " "/etc/trojan/akun.conf" | cut -d ' ' -f 3 | sed -n "${CLIENT_NUMBER}"p)
+sed -i "/^### $user $exp/d" /etc/trojan/akun.conf
+sed -i '/^,"'"$user"'"$/d' /etc/trojan/config.json
+systemctl restart trojan
+service cron restart
 clear
-echo " Trojan Akun berhasil dihapus"
+clear
+echo " Trojan Account Deleted Successfully"
 echo " =========================="
 echo " Client Name : $user"
 echo " Expired On  : $exp"
 echo " =========================="
-echo " Mod by SL"
