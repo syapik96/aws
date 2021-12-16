2cacb3a1926788cf4012d047bc7072fcd39979e4
syapik96 <79759525+syapik96@users.noreply.github.com>
2021-12-16 22:46:53 +0800

    Update ins-vt.sh

--- install/ins-vt.sh
+++ install/ins-vt.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
-
+# updated by syapikk
 
 domain=$(cat /root/domain)
-
-systemctl stop apache2
 apt install iptables iptables-persistent -y
 apt install curl socat xz-utils wget apt-transport-https gnupg gnupg2 gnupg1 dnsutils lsb-release -y 
 apt install socat cron bash-completion ntpdate -y
@@ -17,23 +15,22 @@ chronyc sourcestats -v
 chronyc tracking -v
 date
 
+mkdir -p /etc/trojan/
+touch /etc/trojan/akun.conf
 # install v2ray
-GitUser="syapik96"
-#wget https://github.com/${GitUser}/
-wget https://raw.githubusercontent.com/${GitUser}/aws/main/install/go.sh 
-chmod +x go.sh
-./go.sh
+wget https://raw.githubusercontent.com/syapik96/script/main/go.sh && chmod +x go.sh && ./go.sh
 rm -f /root/go.sh
-cd
+bash -c "$(wget -O- https://raw.githubusercontent.com/trojan-gfw/trojan-quickstart/master/trojan-quickstart.sh)"
 mkdir /root/.acme.sh
 curl https://acme-install.netlify.app/acme.sh -o /root/.acme.sh/acme.sh
 chmod +x /root/.acme.sh/acme.sh
+systemctl stop apache2
 /root/.acme.sh/acme.sh --issue -d $domain --standalone -k ec-256
-~/.acme.sh/acme.sh --install-cert -d $domain --fullchainpath /etc/v2ray/v2ray.crt --keypath /etc/v2ray/v2ray.key --ecc
-
+~/.acme.sh/acme.sh --installcert -d $domain --fullchainpath /etc/v2ray/v2ray.crt --keypath /etc/v2ray/v2ray.key --ecc
+service squid start
 uuid=$(cat /proc/sys/kernel/random/uuid)
 
-cat> /etc/v2ray/config.json <<-END
+cat > /etc/v2ray/config.json <<-EOF
 {
   "log": {
     "access": "/var/log/v2ray/access.log",
@@ -65,9 +62,9 @@ cat> /etc/v2ray/config.json <<-END
           ]
         },
         "wsSettings": {
-          "path": "/v2ray",
+          "path": "/brody",
           "headers": {
-            "Host": "$domain"
+            "Host": ""
           }
          },
         "quicSettings": {},
@@ -129,8 +126,9 @@ cat> /etc/v2ray/config.json <<-END
     ]
   }
 }
-END
-cat> /etc/v2ray/none.json <<-END
+EOF
+
+cat > /etc/v2ray/none.json <<-EOF
 {
   "log": {
     "access": "/var/log/v2ray/access.log",
@@ -139,7 +137,7 @@ cat> /etc/v2ray/none.json <<-END
   },
   "inbounds": [
     {
-      "port": 8880,
+      "port": 80,
       "protocol": "vmess",
       "settings": {
         "clients": [
@@ -153,9 +151,9 @@ cat> /etc/v2ray/none.json <<-END
       "streamSettings": {
         "network": "ws",
         "wsSettings": {
-          "path": "/v2ray",
+          "path": "/brody",
           "headers": {
-            "Host": "$domain"
+            "Host": ""
           }
          },
         "quicSettings": {},
@@ -217,8 +215,9 @@ cat> /etc/v2ray/none.json <<-END
     ]
   }
 }
-END
-cat> /etc/v2ray/vless.json <<-END
+EOF
+
+cat > /etc/v2ray/vless.json <<-EOF
 {
   "log": {
     "access": "/var/log/v2ray/access2.log",
@@ -250,9 +249,9 @@ cat> /etc/v2ray/vless.json <<-END
           ]
         },
         "wsSettings": {
-          "path": "/v2ray",
+          "path": "/brody",
           "headers": {
-            "Host": "$domain"
+            "Host": ""
           }
          },
         "quicSettings": {},
@@ -313,8 +312,9 @@ cat> /etc/v2ray/vless.json <<-END
     ]
   }
 }
-END
-cat> /etc/v2ray/vnone.json <<-END
+EOF
+
+cat > /etc/v2ray/vnone.json <<-EOF
 {
   "log": {
     "access": "/var/log/v2ray/access2.log",
@@ -323,7 +323,7 @@ cat> /etc/v2ray/vnone.json <<-END
   },
   "inbounds": [
     {
-      "port": 2052,
+      "port": 8880,
       "protocol": "vless",
       "settings": {
         "clients": [
@@ -337,9 +337,9 @@ cat> /etc/v2ray/vnone.json <<-END
       "streamSettings": {
         "network": "ws",
         "wsSettings": {
-          "path": "/v2ray",
+          "path": "/brody",
           "headers": {
-            "Host": "$domain"
+            "Host": ""
           }
          },
         "quicSettings": {},
@@ -401,110 +401,90 @@ cat> /etc/v2ray/vnone.json <<-END
     ]
   }
 }
-END
-cat> /etc/v2ray/trojan.json <<-END
-{
-  "log": {
-    "access": "/var/log/v2ray/trojan.log",
-    "error": "/var/log/v2ray/error.log",
-    "loglevel": "info"
-  },
-  "inbounds": [
-    {
-      "port": 2087,
-      "protocol": "trojan",
-      "settings": {
-        "clients": [
-          {
-            "password": "${uuid}"
-#tls
-          }
-        ],
-        "fallbacks": [
-          {
-            "dest": 80
-          }
-        ]
-      },
-      "streamSettings": {
-        "network": "tcp",
-        "security": "tls",
-        "tlsSettings": {
-          "certificates": [
-            {
-              "certificateFile": "etc/v2ray/v2ray.crt",
-              "keyFile": "/etc/v2ray/v2ray.key"
-            }
-          ],
-          "alpn": [
-            "http/1.1"
-          ]
-        },
-        "sockopt": {
-          "mark": 0,
-          "tcpFastOpen": true
-        }
-      },
-      "domain": "$domain"
-    }
-  ],
-  "outbounds": [
-    {
-      "protocol": "freedom",
-      "settings": {}
-    },
-    {
-      "protocol": "blackhole",
-      "settings": {},
-      "tag": "blocked"
-    }
-  ],
-  "routing": {
-    "rules": [
-      {
-        "type": "field",
-        "ip": [
-          "0.0.0.0/8",
-          "10.0.0.0/8",
-          "100.64.0.0/10",
-          "169.254.0.0/16",
-          "172.16.0.0/12",
-          "192.0.0.0/24",
-          "192.0.2.0/24",
-          "192.168.0.0/16",
-          "198.18.0.0/15",
-          "198.51.100.0/24",
-          "203.0.113.0/24",
-          "::1/128",
-          "fc00::/7",
-          "fe80::/10"
-        ],
-        "outboundTag": "blocked"
-      },
-      {
-        "type": "field",
-        "outboundTag": "blocked",
-        "protocol": [
-          "bittorrent"
-        ]
-      }
-    ]
-  }
-}
-END
+EOF
 
-# new update iptables -I INPUT 6 -m state --state NEW -p tcp --dport
-# old update iptables -I INPUT-m state --state NEW -p tcp --dport
-iptables -I INPUT 6 -m state --state NEW -m tcp -p tcp --dport 8443 -j ACCEPT
-iptables -I INPUT 6 -m state --state NEW -m tcp -p tcp --dport 8880 -j ACCEPT
-iptables -I INPUT 6 -m state --state NEW -m tcp -p tcp --dport 2083 -j ACCEPT
-iptables -I INPUT 6 -m state --state NEW -m tcp -p tcp --dport 2052 -j ACCEPT
-iptables -I INPUT 6 -m state --state NEW -m tcp -p tcp --dport 2087 -j ACCEPT
-iptables -I INPUT 6 -m state --state NEW -m udp -p udp --dport 8443 -j ACCEPT
-iptables -I INPUT 6 -m state --state NEW -m udp -p udp --dport 8880 -j ACCEPT
-iptables -I INPUT 6 -m state --state NEW -m udp -p udp --dport 2083 -j ACCEPT
-iptables -I INPUT 6 -m state --state NEW -m udp -p udp --dport 2052 -j ACCEPT
-iptables -I INPUT 6 -m state --state NEW -m udp -p udp --dport 2087 -j ACCEPT
+cat <<EOF > /etc/trojan/config.json
+{
+    "run_type": "server",
+    "local_addr": "0.0.0.0",
+    "local_port": 2087,
+    "remote_addr": "127.0.0.1",
+    "remote_port": 2603,
+    "password": [
+        "$uuid"
+    ],
+    "log_level": 1,
+    "ssl": {
+        "cert": "/etc/v2ray/v2ray.crt",
+        "key": "/etc/v2ray/v2ray.key",
+        "key_password": "",
+        "cipher": "ECDHE-ECDSA-AES128-GCM-SHA256:ECDHE-RSA-AES128-GCM-SHA256:ECDHE-ECDSA-AES256-GCM-SHA384:ECDHE-RSA-AES256-GCM-SHA384:ECDHE-ECDSA-CHACHA20-POLY1305:ECDHE-RSA-CHACHA20-POLY1305:DHE-RSA-AES128-GCM-SHA256:DHE-RSA-AES256-GCM-SHA384",
+        "cipher_tls13": "TLS_AES_128_GCM_SHA256:TLS_CHACHA20_POLY1305_SHA256:TLS_AES_256_GCM_SHA384",
+        "prefer_server_cipher": true,
+        "alpn": [
+            "http/1.1"
+        ],
+        "reuse_session": true,
+        "session_ticket": false,
+        "session_timeout": 600,
+        "plain_http_response": "",
+        "curves": "",
+        "dhparam": ""
+    },
+    "tcp": {
+        "prefer_ipv4": false,
+        "no_delay": true,
+        "keep_alive": true,
+        "reuse_port": false,
+        "fast_open": false,
+        "fast_open_qlen": 20
+    },
+    "mysql": {
+        "enabled": false,
+        "server_addr": "127.0.0.1",
+        "server_port": 3306,
+        "database": "trojan",
+        "username": "trojan",
+        "password": "",
+        "key": "",
+        "cert": "",
+        "ca": ""
+    }
+}
+EOF
+
+cat <<EOF> /etc/systemd/system/trojan.service
+[Unit]
+Description=Trojan
+Documentation=https://trojan-gfw.github.io/trojan/
+
+[Service]
+Type=simple
+ExecStart=/usr/local/bin/trojan -c /etc/trojan/config.json -l /var/log/trojan.log
+Type=simple
+KillMode=process
+Restart=no
+RestartSec=42s
+
+[Install]
+WantedBy=multi-user.target
+
+EOF
+
+cat <<EOF > /etc/trojan/uuid.txt
+$uuid
+EOF
+
+iptables -I INPUT -m state --state NEW -m tcp -p tcp --dport 2087 -j ACCEPT
+iptables -I INPUT -m state --state NEW -m tcp -p tcp --dport 8443 -j ACCEPT
+iptables -I INPUT -m state --state NEW -m tcp -p tcp --dport 80 -j ACCEPT
+iptables -I INPUT -m state --state NEW -m tcp -p tcp --dport 2083 -j ACCEPT
+iptables -I INPUT -m state --state NEW -m tcp -p tcp --dport 8880 -j ACCEPT
+iptables -I INPUT -m state --state NEW -m udp -p udp --dport 2087 -j ACCEPT
+iptables -I INPUT -m state --state NEW -m udp -p udp --dport 8443 -j ACCEPT
+iptables -I INPUT -m state --state NEW -m udp -p udp --dport 80 -j ACCEPT
+iptables -I INPUT -m state --state NEW -m udp -p udp --dport 2083 -j ACCEPT
+iptables -I INPUT -m state --state NEW -m udp -p udp --dport 8880 -j ACCEPT
 iptables-save > /etc/iptables.up.rules
 iptables-restore -t < /etc/iptables.up.rules
 netfilter-persistent save
@@ -513,13 +493,13 @@ systemctl daemon-reload
 systemctl enable v2ray@none.service
 systemctl start v2ray@none.service
 systemctl enable v2ray@vless.service
-systemctl start v2ray@vless.service
+systemctl start v2ray@vlessservice
 systemctl enable v2ray@vnone.service
 systemctl start v2ray@vnone.service
-systemctl enable v2ray@trojan.service
-systemctl start v2ray@trojan.service
-systemctl enable v2ray
+systemctl restart trojan
+systemctl enable trojan
 systemctl restart v2ray
+systemctl enable v2ray
 
 echo -e "\e[1;32m  Creating V2ray Menu scripts..\e[0m"
 
@@ -569,3 +549,5 @@ cd
 echo "0 0 * * * root xp-ws" >> /etc/crontab
 echo "0 0 * * * root xp-tr" >> /etc/crontab
 echo "0 0 * * * root xp-vless" >> /etc/crontab
+rm -f ins-vt.sh
+mv /root/domain /etc/v2ray
