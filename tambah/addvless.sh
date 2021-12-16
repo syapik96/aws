b8c8c6425a2a5501e879a20a12349ef7a3beb2ba
syapik96 <79759525+syapik96@users.noreply.github.com>
2021-12-16 22:18:18 +0800

    Update addvless.sh

--- tambah/addvless.sh
+++ tambah/addvless.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+# updated by syapikk96
+
 source /var/lib/premium-script/ipvps.conf
 if [[ "$IP" = "" ]]; then
 domain=$(cat /etc/v2ray/domain)
@@ -27,8 +29,8 @@ sed -i '/#tls$/a\### '"$user $exp"'\
 },{"id": "'""$uuid""'","email": "'""$user""'"' /etc/v2ray/vless.json
 sed -i '/#none$/a\### '"$user $exp"'\
 },{"id": "'""$uuid""'","email": "'""$user""'"' /etc/v2ray/vnone.json
-vlesslink1="vless://${uuid}@${domain}:5443?path=/v2ray&security=tls&encryption=none&type=ws#${user}"
-vlesslink2="vless://${uuid}@${domain}:880?path=/v2ray&encryption=none&type=ws#${user}"
+vlesslink1="vless://${uuid}@${domain}:2083?path=/v2ray&security=tls&encryption=none&type=ws#${user}"
+vlesslink2="vless://${uuid}@${domain}:2052?path=/v2ray&encryption=none&type=ws#${user}"
 systemctl restart v2ray@vless
 systemctl restart v2ray@vnone
 clear
@@ -37,8 +39,8 @@ echo -e "==========-V2RAY/VLESS-=========="
 echo -e "Remarks        : ${user}"
 echo -e "Domain         : ${domain}"
 echo -e "Server IP      : $PUBLIC_IP"
-echo -e "port TLS       : 5443"
-echo -e "port none TLS  : 880"
+echo -e "port TLS       : 2083"
+echo -e "port none TLS  : 2052"
 echo -e "id             : ${uuid}"
 echo -e "Encryption     : none"
 echo -e "network        : ws"
