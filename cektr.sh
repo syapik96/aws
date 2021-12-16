b7ca5db61e0319d4974d9be4ac4fed5735a5078e
syapik96 <79759525+syapik96@users.noreply.github.com>
2021-12-16 22:50:15 +0800

    Update cektr.sh

--- cektr.sh
+++ cektr.sh
@@ -1,33 +1,28 @@
 #!/bin/bash
-data=( `cat /etc/v2ray/trojan.json | grep '^###' | cut -d ' ' -f 2`);
+
+red='\e[1;31m'
+green='\e[0;32m'
+NC='\e[0m'
+clear
+data=( `cat /var/log/trojan.log | grep -w 'authenticated as' | awk '{print $7}' | sort | uniq`);
 echo "-------------------------------";
 echo "-----=[ Trojan User Login ]=-----";
 echo "-------------------------------";
 for akun in "${data[@]}"
 do
-if [[ -z "$akun" ]]; then
-akun="tidakada"
-fi
-data2=( `netstat -anp | grep ESTABLISHED | grep tcp6 | grep v2ray | awk '{print $5}' | cut -d: -f1 | sort | uniq`);
+data2=( `lsof -n | grep -i ESTABLISHED | grep trojan | awk '{print $9}' | cut -d':' -f2 | grep -w 445 | cut -d- -f2 | grep -v '>127.0.0.1' | sort | uniq | cut -d'>' -f2`);
 echo -n > /tmp/iptrojan.txt
 for ip in "${data2[@]}"
 do
-jum=$(cat /var/log/v2ray/trojan.log | grep -w $akun | awk '{print $3}' | cut -d: -f1 | grep -w $ip | sort | uniq)
+jum=$(cat /var/log/trojan.log | grep -w $akun | awk '{print $4}' | cut -d: -f1 | grep -w $ip | sort | uniq)
 if [[ -z "$jum" ]]; then
 echo > /dev/null
 else
-echo "$jum" >> /tmp/iptrojan.txt
+echo "$jum" > /tmp/iptrojan.txt
 fi
 done
-jum=$(cat /tmp/iptrojan.txt)
-if [[ -z "$jum" ]]; then
-echo > /dev/null
-else
 jum2=$(cat /tmp/iptrojan.txt | nl)
 echo "user : $akun";
 echo "$jum2";
 echo "-------------------------------"
-fi
 done
-echo "Mod by SL"
-
