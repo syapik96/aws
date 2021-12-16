72921fdbbd5060ecbac4ae09c8d257a6b8f7b0f2
syapik96 <79759525+syapik96@users.noreply.github.com>
2021-12-16 22:48:14 +0800

    Update addtr.sh

--- tambah/addtr.sh
+++ tambah/addtr.sh
@@ -1,47 +1,44 @@
 #!/bin/bash
-# use this systemctl restart trojan if default not work ( let me know which one work )
+
 # updated 
 
 red='\e[1;31m'
 green='\e[0;32m'
 NC='\e[0m'
 clear
+uuid=$(cat /etc/trojan/uuid.txt)
 source /var/lib/premium-script/ipvps.conf
 if [[ "$IP" = "" ]]; then
 domain=$(cat /etc/v2ray/domain)
 else
 domain=$IP
 fi
-until [[ $user =~ ^[a-zA-Z0-9_]+$ && ${CLIENT_EXISTS} == '0' ]]; do
-		read -rp "UserPassword: " -e user
-		CLIENT_EXISTS=$(grep -w $user /etc/v2ray/trojan.json | wc -l)
+tr="$(cat ~/log-install.txt | grep -i Trojan | cut -d: -f2|sed 's/ //g')"
+until [[ $user =~ ^[a-zA-Z0-9_]+$ && ${user_EXISTS} == '0' ]]; do
+		read -rp "Password: " -e user
+		user_EXISTS=$(grep -w $user /etc/trojan/akun.conf | wc -l)
 
-		if [[ ${CLIENT_EXISTS} == '1' ]]; then
-			echo -e "\e[1;31m"
-			echo -e "A client with the specified name was already created, please choose another name."
-			echo -e "\e[0m"
+		if [[ ${user_EXISTS} == '1' ]]; then
+			echo ""
+			echo "A client with the specified name was already created, please choose another name."
 			exit 1
 		fi
 	done
-uuid=$(cat /proc/sys/kernel/random/uuid)	
 read -p "Expired (days): " masaaktif
-sed -i '/#tls$/a\### '"$user $exp"'\
-},{"password": "'""$uuid""'","email": "'""$user""'"' /etc/v2ray/trojan.json
+sed -i '/"'""$uuid""'"$/a\,"'""$user""'"' /etc/trojan/config.json
 exp=`date -d "$masaaktif days" +"%Y-%m-%d"`
-systemctl restart v2ray@trojan
-# systemctl restart trojan
-trojanlink="trojan://${uuid}@${domain}:6443"
-
+echo -e "### $user $exp" >> /etc/trojan/akun.conf
+systemctl restart trojan
+trojanlink="trojan://${user}@${domain}:${tr}"
 clear
 echo -e ""
-echo -e "==========-Trojan-=========="
+echo -e "=============-Trojan-============"
 echo -e "Remarks        : ${user}"
 echo -e "Host/IP        : ${domain}"
-echo -e "port           : 6443"
-echo -e "Key            : ${uuid}"
+echo -e "port           : ${tr}"
+echo -e "Key            : ${user}"
 echo -e "link           : ${trojanlink}"
 echo -e "================================="
 echo -e "Expired On     : $exp"
-echo -e ""
-echo -e "Mod By OnePiece"
+
 
