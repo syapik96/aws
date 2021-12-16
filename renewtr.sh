9a0bdf42595849107120ad0418d27e39be2cbd33
syapik96 <79759525+syapik96@users.noreply.github.com>
2021-12-16 22:54:30 +0800

    Update renewtr.sh

--- renewtr.sh
+++ renewtr.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-NUMBER_OF_CLIENTS=$(grep -c -E "^### " "/etc/v2ray/trojan.json")
+clear
+NUMBER_OF_CLIENTS=$(grep -c -E "^### " "/etc/trojan/akun.conf")
 	if [[ ${NUMBER_OF_CLIENTS} == '0' ]]; then
 		clear
 		echo ""
@@ -12,7 +13,7 @@ NUMBER_OF_CLIENTS=$(grep -c -E "^### " "/etc/v2ray/trojan.json")
 	echo "Select the existing client you want to renew"
 	echo " Press CTRL+C to return"
 	echo -e "==============================="
-	grep -E "^### " "/etc/v2ray/trojan.json" | cut -d ' ' -f 2-3 | nl -s ') '
+	grep -E "^### " "/etc/trojan/akun.conf" | cut -d ' ' -f 2-3 | nl -s ') '
 	until [[ ${CLIENT_NUMBER} -ge 1 && ${CLIENT_NUMBER} -le ${NUMBER_OF_CLIENTS} ]]; do
 		if [[ ${CLIENT_NUMBER} == '1' ]]; then
 			read -rp "Select one client [1]: " CLIENT_NUMBER
@@ -21,21 +22,20 @@ NUMBER_OF_CLIENTS=$(grep -c -E "^### " "/etc/v2ray/trojan.json")
 		fi
 	done
 read -p "Expired (days): " masaaktif
-user=$(grep -E "^### " "/etc/v2ray/trojan.json" | cut -d ' ' -f 2 | sed -n "${CLIENT_NUMBER}"p)
-exp=$(grep -E "^### " "/etc/v2ray/trojan.json" | cut -d ' ' -f 3 | sed -n "${CLIENT_NUMBER}"p)
+user=$(grep -E "^### " "/etc/trojan/akun.conf" | cut -d ' ' -f 2 | sed -n "${CLIENT_NUMBER}"p)
+exp=$(grep -E "^### " "/etc/trojan/akun.conf" | cut -d ' ' -f 3 | sed -n "${CLIENT_NUMBER}"p)
 now=$(date +%Y-%m-%d)
 d1=$(date -d "$exp" +%s)
 d2=$(date -d "$now" +%s)
 exp2=$(( (d1 - d2) / 86400 ))
 exp3=$(($exp2 + $masaaktif))
 exp4=`date -d "$exp3 days" +"%Y-%m-%d"`
-sed -i "s/### $user $exp/### $user $exp4/g" /etc/v2ray/trojan.json
+sed -i "s/### $user $exp/### $user $exp4/g" /etc/trojan/akun.conf
 clear
 echo ""
-echo " Akun Trojan berhasil diperpanjang"
+echo " Trojan Account Was Successfully Renewed"
 echo " =========================="
 echo " Client Name : $user"
 echo " Expired On  : $exp4"
 echo " =========================="
-echo " Mod by SL"
 
