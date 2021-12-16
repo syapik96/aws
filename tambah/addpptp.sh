053516350fc6e9263d610a4ccaa444ebeb4e28fd
syapik96 <79759525+syapik96@users.noreply.github.com>
2021-12-16 20:16:56 +0800

    Update addpptp.sh

--- tambah/addpptp.sh
+++ tambah/addpptp.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-source /var/lib/crot-script/ipvps.conf
+source /var/lib/premium-script/ipvps.conf
 if [[ "$IP" = "" ]]; then
 PUBLIC_IP=$(wget -qO- http://ipecho.net/plain | xargs echo);
 else
@@ -7,7 +7,7 @@ PUBLIC_IP=$IP
 fi
 until [[ $VPN_USER =~ ^[a-zA-Z0-9_]+$ && ${CLIENT_EXISTS} == '0' ]]; do
 		read -rp "Username: " -e VPN_USER
-		CLIENT_EXISTS=$(grep -w $VPN_USER /var/lib/crot-script/data-user-pptp | wc -l)
+		CLIENT_EXISTS=$(grep -w $VPN_USER /var/lib/premium-script/data-user-pptp | wc -l)
 
 		if [[ ${CLIENT_EXISTS} == '1' ]]; then
 			echo ""
@@ -21,14 +21,14 @@ exp=`date -d "$masaaktif days" +"%Y-%m-%d"`
 clear
 
 # Add or update VPN user
-cat >> /etc/ppp/chap-secrets <<EOF
+cat > /etc/ppp/chap-secrets <<-EOF
 "$VPN_USER" pptpd "$VPN_PASSWORD" *
 EOF
 
 # Update file attributes
 chmod 600 /etc/ppp/chap-secrets*
-echo -e "### $VPN_USER $exp">>"/var/lib/crot-script/data-user-pptp"
-cat <<EOF
+echo -e "### $VPN_USER $exp">>"/var/lib/premium-script/data-user-pptp"
+cat <<-EOF
 
 ================================
 PPTP VPN
@@ -39,6 +39,6 @@ Username     : $VPN_USER
 Password     : $VPN_PASSWORD
 Expired On   : $exp
 
-Mod By oNEpIECE
+Mod By Prince
 =================================
 EOF
