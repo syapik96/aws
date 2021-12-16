982c88d8621c522481d8198956bbbf741000854e
syapik96 <79759525+syapik96@users.noreply.github.com>
2021-12-16 20:39:06 +0800

    Update addsstp.sh

--- tambah/addsstp.sh
+++ tambah/addsstp.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
+# updated by syapik96
+
 IP=$(wget -qO- http://ipecho.net/plain | xargs echo);
 until [[ $user =~ ^[a-zA-Z0-9_]+$ && ${CLIENT_EXISTS} == '0' ]]; do
 		read -rp "Usernew: " -e user
-		CLIENT_EXISTS=$(grep -w $user /var/lib/crot-script/data-user-sstp | wc -l)
+		CLIENT_EXISTS=$(grep -w $user /var/lib/premium-script/data-user-sstp | wc -l)
 
 		if [[ ${CLIENT_EXISTS} == '1' ]]; then
 			echo ""
@@ -13,12 +15,12 @@ until [[ $user =~ ^[a-zA-Z0-9_]+$ && ${CLIENT_EXISTS} == '0' ]]; do
 read -p "Password: " pass
 read -p "Expired (days): " masaaktif
 exp=`date -d "$masaaktif days" +"%Y-%m-%d"`
-cat >> /home/sstp/sstp_account <<EOF
+cat > /home/sstp/sstp_account <<-EOF
 $user * $pass *
 EOF
-echo -e "### $user $exp">>"/var/lib/crot-script/data-user-sstp"
+echo -e "### $user $exp">>"/var/lib/premium-script/data-user-sstp"
 clear
-cat <<EOF
+cat <<-EOF
 
 echo -e "=============== SSTP VPN ================="
 
