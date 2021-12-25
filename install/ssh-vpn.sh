0f95469df7ed1c4a1d22b2fc049fe315dfafdfc8
syapik96 <79759525+syapik96@users.noreply.github.com>
2021-12-25 12:20:43 +0800

    Update ssh-vpn.sh

--- install/ssh-vpn.sh
+++ install/ssh-vpn.sh
@@ -8,7 +8,7 @@ GitUser="syapik96"
 
 # initializing var
 export DEBIAN_FRONTEND=noninteractive
-MYIP=$(wget -qO- ipinfo.io/ip);
+MYIP=$(wget -qO- icanhazip.com);
 MYIP2="s/xxxxxxxxx/$MYIP/g";
 NET=$(ip -o $ANU -4 route show to default | awk '{print $5}');
 source /etc/os-release
