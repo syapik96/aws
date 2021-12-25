92b393b6ed9758ecd07ee33280034208543075c8
syapik96 <79759525+syapik96@users.noreply.github.com>
2021-12-26 06:52:59 +0800

    Update addvless.sh

--- tambah/addvless.sh
+++ tambah/addvless.sh
@@ -3,14 +3,14 @@
 
 source /var/lib/premium-script/ipvps.conf
 if [[ "$IP" = "" ]]; then
-domain=$(cat /etc/v2ray/domain)
+   domain=$(cat /etc/v2ray/domain)
 else
-domain=$IP
+   domain=$IP
 fi
 if [[ "$IPHost" = "" ]]; then
-PUBLIC_IP=$(wget -qO- icanhazip.com);
+   PUBLIC_IP=$(wget -qO- icanhazip.com);
 else
-PUBLIC_IP=$IPHost
+   PUBLIC_IP=$IPHost
 fi
 until [[ $user =~ ^[a-zA-Z0-9_]+$ && ${CLIENT_EXISTS} == '0' ]]; do
 		read -rp "User: " -e user
@@ -18,7 +18,7 @@ until [[ $user =~ ^[a-zA-Z0-9_]+$ && ${CLIENT_EXISTS} == '0' ]]; do
 
 		if [[ ${CLIENT_EXISTS} == '1' ]]; then
 			echo ""
-			echo "A client with the specified name was already created, please choose another name."
+			echo -e "A client with the specified name was already created, please choose another name."
 			exit 1
 		fi
 	done
@@ -29,7 +29,7 @@ sed -i '/#tls$/a\### '"$user $exp"'\
 },{"id": "'""$uuid""'","email": "'""$user""'"' /etc/v2ray/vless.json
 sed -i '/#none$/a\### '"$user $exp"'\
 },{"id": "'""$uuid""'","email": "'""$user""'"' /etc/v2ray/vnone.json
-vlesslink1="vless://${uuid}@${domain}:2083?path=/v2ray&security=tls&encryption=none&type=ws#${user}"
+vlesslink1="vless://${uuid}@${domain}:2082?path=/v2ray&security=tls&encryption=none&type=ws#${user}"
 vlesslink2="vless://${uuid}@${domain}:2052?path=/v2ray&encryption=none&type=ws#${user}"
 systemctl restart v2ray@vless
 systemctl restart v2ray@vnone
@@ -40,7 +40,7 @@ echo -e ""
 echo -e "Remarks        : ${user}"
 echo -e "Domain         : ${domain}"
 echo -e "Server IP      : $PUBLIC_IP"
-echo -e "port TLS       : 2083"
+echo -e "port TLS       : 2082"
 echo -e "port none TLS  : 2052"
 echo -e "id             : ${uuid}"
 echo -e "Encryption     : none"
@@ -53,4 +53,4 @@ echo -e "link none TLS  : ${vlesslink2}"
 echo -e "================================="
 echo -e "Expired On     : $exp"
 echo -e ""
-echo -e "Mod By Princenewbie"
+echo -e "By Princenewbie"
