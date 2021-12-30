5d75f7ceef3c41de77b108e3141edbc8e5acfe96
syapik96 <79759525+syapik96@users.noreply.github.com>
2021-12-30 15:44:25 +0800

    Update dell2tp.sh

--- hapus/dell2tp.sh
+++ hapus/dell2tp.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
+# Modded by Prince
+
 export PATH="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin"
-NUMBER_OF_CLIENTS=$(grep -c -E "^### " "/var/lib/crot-script/data-user-l2tp")
+NUMBER_OF_CLIENTS=$(grep -c -E "^### " "/var/lib/premium-script/data-user-l2tp")
 	if [[ ${NUMBER_OF_CLIENTS} == '0' ]]; then
 		echo ""
 		echo "You have no existing clients!"
@@ -12,7 +14,7 @@ NUMBER_OF_CLIENTS=$(grep -c -E "^### " "/var/lib/crot-script/data-user-l2tp")
 	echo " Press CTRL+C to return"
 	echo " ==============================="
 	echo "     No  Expired   User"
-	grep -E "^### " "/var/lib/crot-script/data-user-l2tp" | cut -d ' ' -f 2-3 | nl -s ') '
+	grep -E "^### " "/var/lib/premium-script/data-user-l2tp" | cut -d ' ' -f 2-3 | nl -s ') '
 	until [[ ${CLIENT_NUMBER} -ge 1 && ${CLIENT_NUMBER} -le ${NUMBER_OF_CLIENTS} ]]; do
 		if [[ ${CLIENT_NUMBER} == '1' ]]; then
 			read -rp "Select One Client[1]: " CLIENT_NUMBER
@@ -21,19 +23,23 @@ NUMBER_OF_CLIENTS=$(grep -c -E "^### " "/var/lib/crot-script/data-user-l2tp")
 		fi
 	done
 	# match the selected number to a client name
-	VPN_USER=$(grep -E "^### " "/var/lib/crot-script/data-user-l2tp" | cut -d ' ' -f 2 | sed -n "${CLIENT_NUMBER}"p)
-	exp=$(grep -E "^### " "/var/lib/crot-script/data-user-l2tp" | cut -d ' ' -f 3 | sed -n "${CLIENT_NUMBER}"p)
+	VPN_USER=$(grep -E "^### " "/var/lib/premium-script/data-user-l2tp" | cut -d ' ' -f 2 | sed -n "${CLIENT_NUMBER}"p)
+	exp=$(grep -E "^### " "/var/lib/premium-script/data-user-l2tp" | cut -d ' ' -f 3 | sed -n "${CLIENT_NUMBER}"p)
 # Delete VPN user
 sed -i '/^"'"$VPN_USER"'" l2tpd/d' /etc/ppp/chap-secrets
 # shellcheck disable=SC2016
 sed -i '/^'"$VPN_USER"':\$1\$/d' /etc/ipsec.d/passwd
-sed -i "/^### $VPN_USER $exp/d" /var/lib/crot-script/data-user-l2tp
+sed -i "/^### $VPN_USER $exp/d" /var/lib/premium-script/data-user-l2tp
 # Update file attributes
 chmod 600 /etc/ppp/chap-secrets* /etc/ipsec.d/passwd*
 clear
-echo " L2TP/IPSEC PSK User Akun berhasil dihapus"
-echo " =========================="
-echo " Client Name : $VPN_USER"
-echo " Expired On  : $exp"
-echo " =========================="
-echo " Mod by SL"
+figlet -f slant "Delete Done" | lolcat
+echo -e ""
+echo -e " \e[1;31mL2TP/IPSEC PSK User Akun berhasil dihapus\e[0m"
+echo -e " =========================================" | lolcat
+echo -e ""
+echo -e "       \e[1;31mClient Name :\e[0m \e[1;32m$VPN_USER\e[0m"
+echo -e "       \e[1:31mExpired On  :\e[0m \e[1;32m$exp\e[0m"
+echo -e ""
+echo -e " ==========================================" | lolcat
+echo -e ""
