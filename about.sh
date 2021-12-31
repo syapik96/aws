4d406f988d5320d568e87e582da7c8c80559d28c
syapik96 <79759525+syapik96@users.noreply.github.com>
2021-12-31 17:28:54 +0800

    Update about.sh

--- about.sh
+++ about.sh
@@ -1,26 +1,43 @@
 #!/bin/bash
-clear
-figlet "Premium Autoscript Vps" | lolcat
-echo -e "#====================================================================#" | lolcat       
-echo -e "#                   Premium Script By OnePieceVPN                    #"  | lolcat    
-echo -e "#                                                                    #"  | lolcat    
-echo -e "#       ░█▀▀▀█ ░█▄─░█ ░█▀▀▀ 　 ░█▀▀█ ▀█▀ ░█▀▀▀ ░█▀▀█ ░█▀▀▀           #" | lolcat    
-echo -e "#       ░█──░█ ░█░█░█ ░█▀▀▀ 　 ░█▄▄█ ░█─ ░█▀▀▀ ░█─── ░█▀▀▀           #"  | lolcat                  
-echo -e "#       ░█▄▄▄█ ░█──▀█ ░█▄▄▄ 　 ░█─── ▄█▄ ░█▄▄▄ ░█▄▄█ ░█▄▄▄           #"   | lolcat          
-echo -e "#                                                                    #"   | lolcat   
-echo -e "#                           - Modded By PrinceNewbie                 #"  | lolcat 
-echo -e "#           │▒│ /▒/         - Thanks To :                            #"  | lolcat  
-echo -e "#           │▒│/▒/          - Whatsapp  :                           #"  | lolcat     
-echo -e "#         　│▒ /▒/─┬─┐      - Telegram : t.me/PrinceNewbie           #"   | lolcat    
-echo -e "#           │▒│▒|▒│▒│       - To those who use this script,          #"   | lolcat     
-echo -e "#          ┌┴─┴─┐-┘─┘         this script is still under development #" | lolcat
-echo -e "#          │▒┌──┘▒▒▒│        - Thanks for who using this script       #" | lolcat      
-echo -e "#          └┐▒▒▒▒▒▒┌┘       - To EnterMenuList = 'menu'              #"   | lolcat    
-echo -e "#                                                                    #"  | lolcat     
-echo -e "#====================================================================#" | lolcat
+#Script Mod by Prince AKA syapik96
 
-echo -e "                         COPYRIGHT © 2021 "    
-echo -e "               ONEPIECEVPN POWERED BY PRINCENEWBIE"
-echo -e "            Privacy Policy Terms & Conditions Disclaimer"  
-sleep 10
-ssh
+clear
+figlet -f slant "Free Autoscript" | lolcat
+echo -e "#====================================================================#" | lolcat
+echo -e "#                   Premium Script By OnePieceVPN                    #" | lolcat
+echo -e "#                                                                    #" | lolcat
+echo -e "#       ░█▀▀▀█ ░█▄─░█ ░█▀▀▀ 　 ░█▀▀█ ▀█▀ ░█▀▀▀ ░█▀▀█ ░█▀▀▀           #" | lolcat
+echo -e "#       ░█──░█ ░█░█░█ ░█▀▀▀ 　 ░█▄▄█ ░█─ ░█▀▀▀ ░█─── ░█▀▀▀           #" | lolcat
+echo -e "#       ░█▄▄▄█ ░█──▀█ ░█▄▄▄ 　 ░█─── ▄█▄ ░█▄▄▄ ░█▄▄█ ░█▄▄▄           #" | lolcat
+echo -e "#                                                                    #" | lolcat
+echo -e "#                         - Modded By PrinceNewbie                   #" | lolcat
+echo -e "#           │▒│ /▒/       - Thanks To :                              #" | lolcat
+echo -e "#           │▒│/▒/        - Whatsapp  :                              #" | lolcat
+echo -e "#         　│▒ /▒/─┬─┐    - Telegram : t.me/PrinceNewbie             #" | lolcat
+echo -e "#           │▒│▒|▒│▒│     - To those who use this script,            #" | lolcat
+echo -e "#          ┌┴─┴─┐-┘─┘       this script is still under development   #" | lolcat
+echo -e "#          │▒┌──┘▒▒▒│     - Thanks for who using this script         #" | lolcat
+echo -e "#          └┐▒▒▒▒▒▒┌┘     - To EnterMenuList = 'menu'                #" | lolcat
+echo -e "#                                                                    #" | lolcat
+echo -e "#====================================================================#" | lolcat
+echo -e ""
+echo -e "                         \e[1;32mCOPYRIGHT © 2021\e[0m "
+echo -e "               \e[1;32mONEPIECEVPN POWERED BY PRINCENEWBIE\e[0m"
+echo -e "            \e[1;32mPrivacy Policy Terms & Conditions Disclaimer\e[0m"
+echo -e "\e[1;31m"
+echo -e "                Pres Enter To back Menu or X To Exit "
+echo -e "\e[1;34m"
+read -p "    Enter choose key : " dua
+echo -e "\e[0m"
+case $dua in
+        *)
+        clear
+        menu
+        exit
+        ;;
+        X)
+        clear
+        exit
+        ;;
+    esac
+
