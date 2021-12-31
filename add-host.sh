26094b3f194c7535bfe2dac1c1d0b1b444b994d6
syapik96 <79759525+syapik96@users.noreply.github.com>
2021-12-31 16:58:52 +0800

    Update add-host.sh

--- add-host.sh
+++ add-host.sh
@@ -2,11 +2,27 @@
 ### 🔰 COPYRIGHT © 2021 OnePieceVPN, Inc 🔰 ###
 ### Script Update by : t.me/PrinceNewbie ####
 
-echo -e "\e[1;31m PASTIKAN DOMAIN SUDAH POINTING IP VPS ANDA \e[0m"
-echo -e "\e[1;31mJika Tiada domain sila Create Domain dahulu" 
-echo -e "Contonya di Freenom(free) \e[0m" 
+red='\e[1;31m'
+green='\e[1;32m'
+yellow='\e[1;33m'
+blue='\e[1;34m'
+NC='\e[0m'
 
-read -rp "Domain / Host : " -e host
+clear
+cd
+rm /root/domain
+rm /etc/v2ray/domain
+rm /var/lib/premium-script/ipvps.conf
+echo -e "\e[1;105m==========================================================\e[0m"
+echo -e ""
+echo -e "${red}Ensure the Domain is already routed to cloudflare (proxy off)${NC}"
+echo -e "${green}Enter your domain , if you do not currently have a domain"
+echo -e "or not route with cloudeflare yet click enter to add again later${NC}"
+echo -e ""
+echo -e "\e[1;105m==========================================================\e[0m" 
+echo -e "${blue}"
+read -rp "   Domain / Host : " -e host
 echo "IP=$host" > /var/lib/premium-script/ipvps.conf
 echo "$host" > /etc/v2ray/domain
 echo "$host" > /root/domain
+echo -e "${NC}"
