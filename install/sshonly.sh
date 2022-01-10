a175e5f151745ca99fba54278132120970c8a53c
syapik96 <79759525+syapik96@users.noreply.github.com>
2022-01-10 20:55:47 +0800

    Update sshonly.sh

--- install/sshonly.sh
+++ install/sshonly.sh
@@ -13,12 +13,14 @@ echo "${red}You need to run this script as root${NC}"
 exit 1
 fi
 
-if [[ ! -e mkdir /var/lib/premium-script ]]; then
-    rm -rf /var/lib/premium-script
+if [[ ! -e /var/lib/premium-script ]]; then
+rm -rf /var/lib/premium-script
+mkdir /var/lib/premium-script
 else
-  mkdir /var/lib/premium-script
-  #touch /var/lib/premium-script/ipvps.conf
- fi
+mkdir /var/lib/premium-script
+#touch /var/lib/premium-script/ipvps.conf
+fi
+
 clear
 echo -e "\e[1;32mMasukkan Domain Anda, Jika Anda Tidak Memiliki Domain Klik Enter"
 echo -e "Ketikkan Perintah addhost setelah proses instalasi Script Selesai\e[0m"
@@ -26,27 +28,26 @@ echo -e "\e[1;31m"
 read -p "Hostname / Domain: " host
 echo -e "\e[0m"
 echo "IP=$host" >> /var/lib/premium-script/ipvps.conf
-echo "$host" >> /etc/v2ray/domain
 echo "$host" >> /root/domain
 GitUser="syapik96"
 wget https://raw.githubusercontent.com/${GitUser}/aws/main/install/hanyassh.sh && chmod +x hanyassh.sh && screen -S hanyassh.sh ./hanyassh.sh
-wget https://raw.githubusercontent.com/${GitUser}/aws/main/websocket-python/websocket.sh && chmod +x websocket.sh && screen -S websocket.sh ./websocket.sh
+wget https://raw.githubusercontent.com/${GitUser}/aws/main/websocketsshonly.sh && chmod +x websocketsshonly.sh && screen -S websocketsshonly.sh ./websocket.sh
 rm -f /root/hanyassh.sh
-rm -f /root/websocket.sh
+rm -f /root/websocketsshonly.sh
 history -c
 echo "1.1" > /home/ver
 clear
 echo " "
 echo "Installation has been completed!!"
 echo " "
-echo "=================================-Script Mod SL YNTKTS-===========================" | tee -a log-install.txt
+echo "=================================-Script Mod Prince96-===========================" | tee -a log-install.txt
 echo "" | tee -a log-install.txt
 echo "----------------------------------------------------------------------------------" | tee -a log-install.txt
 echo ""  | tee -a log-install.txt
 echo "   >>> Service & Port"  | tee -a log-install.txt
 echo "   - OpenSSH                  : 22, 500, X40000"  | tee -a log-install.txt
 echo "   - SSH-WS Python OpenSSH    : 100"  | tee -a log-install.txt
-echo "   - SSH-WS Python Dropbear   : 69"  | tee -a log-install.txt
+echo "   - SSH-WS Python Dropbear   : 80"  | tee -a log-install.txt
 echo "   - SSH-WS Python SSL/TLS    : 443"  | tee -a log-install.txt
 echo "   - Stunnel4 SSL/TLS         : 99, X30000"  | tee -a log-install.txt
 echo "   - Dropbear                 : 143, 109, X50000"  | tee -a log-install.txt
@@ -54,7 +55,7 @@ echo "   - Badvpn                   : 7100, 7200, 7300"  | tee -a log-install.tx
 echo "   - Nginx                    : 81"  | tee -a log-install.txt
 echo ""  | tee -a log-install.txt
 echo "   >>> Server Information & Other Features"  | tee -a log-install.txt
-echo "   - Timezone                : Asia/Kuala_Lumpur/Singapore (GMT +7)"  | tee -a log-install.txt
+echo "   - Timezone                : Asia/Kuala_Lumpur (GMT +8)"  | tee -a log-install.txt
 echo "   - Fail2Ban                : [ON]"  | tee -a log-install.txt
 echo "   - Dflate                  : [ON]"  | tee -a log-install.txt
 echo "   - IPtables                : [ON]"  | tee -a log-install.txt
@@ -63,7 +64,7 @@ echo "   - IPv6                    : [OFF]"  | tee -a log-install.txt
 echo "   - Autoreboot On 00.00 GMT +8" | tee -a log-install.txt
 echo "   - Installation Log --> /root/log-install.txt"  | tee -a log-install.txt
 echo ""  | tee -a log-install.txt
-echo "------------------------------------------Mod by SL------------------------------------------" | tee -a log-install.txt
+echo "------------------------------------------Mod by Prince96------------------------------------------" | tee -a log-install.txt
 echo "Installation Success"
 echo " Reboot in 5 Sec"
 sleep 5
