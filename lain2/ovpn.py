1eb97258b5e12190d032fd8d97f01ac42472fbe4
syapik96 <79759525+syapik96@users.noreply.github.com>
2022-01-20 00:59:23 +0800

    Update ovpn.py

--- lain2/ovpn.py
+++ lain2/ovpn.py
@@ -2,17 +2,19 @@
 import socket, threading, thread, select, signal, sys, time, getopt
 
 # Listen
-LISTENING_ADDR = '0.0.0.0'
-LISTENING_PORT = sys.argv[1]
-
+LISTENING_ADDR = '127.0.0.1'
+if sys.argv[1:]:
+  LISTENING_PORT = sys.argv[1]
+else:
+  LISTENING_PORT = 2099 
 #Pass
 PASS = ''
 
 # CONST
 BUFLEN = 4096 * 4
 TIMEOUT = 60
-DEFAULT_HOST = '127.0.0.1:1194'
-RESPONSE = 'HTTP/1.1 101 <i><u><font color="red">_Brody_</font></u>\r\n\r\nContent-Length: 104857600000\r\n\r\n'
+DEFAULT_HOST = '127.0.0.1:109'
+RESPONSE = 'HTTP/1.1 101 <i><u><font color="red">Switching Protocols</font></u></i>\r\nContent-Length: 104857600000\r\n\r\n'
 
 class Server(threading.Thread):
     def __init__(self, host, port):
