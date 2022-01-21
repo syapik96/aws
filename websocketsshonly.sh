670c3d302380e2e94c5d3198231866c83f3ce44d
syapik96 <79759525+syapik96@users.noreply.github.com>
2022-01-22 02:38:53 +0800

    Update websocketsshonly.sh

--- websocketsshonly.sh
+++ websocketsshonly.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
-#
+# UPDATED 22-1-22
 
-# Edu OVPN
+cd 
+# Installing Service ws edu-ovpn
 wget -q -O /usr/local/bin/edu-ovpn "https://raw.githubusercontent.com/syapik96/aws/main/lain2/ovpn.py"
 chmod +x /usr/local/bin/edu-ovpn
 
-# Installing Service
+# Create system Service ws edu-ovpn
 cat > /etc/systemd/system/edu-ovpn.service <<END
 [Unit]
 Description=Edu-Ovpn Python Over Websocket 
-Documentation=https://gilergames.tk
+Documentation=https://github.com/syapik96/aws
 After=network.target nss-lookup.target
 
 [Service]
@@ -25,19 +26,15 @@ Restart=on-failure
 WantedBy=multi-user.target
 END
 
-systemctl daemon-reload
-systemctl enable edu-ovpn
-systemctl restart edu-ovpn
-
-# ws-dropbear
+# Installing Service ws-dropbear
 wget -q -O /usr/local/bin/ws-dropbear "https://raw.githubusercontent.com/syapik96/aws/main/lain2/dropbear-ws.py"
 chmod +x /usr/local/bin/ws-dropbear
 
-# Installing Service
-cat > /etc/systemd/system/ws-dropbear.service <<-END
+# Create system Service ws-dropbear
+cat > /etc/systemd/system/ws-dropbear.service <<END
 [Unit]
 Description=Dropbear Python Over Websocket
-Documentation=https://gilergames.tk
+Documentation=https://github.com/syapik96/aws
 After=network.target nss-lookup.target
 
 [Service]
@@ -53,19 +50,15 @@ Restart=on-failure
 WantedBy=multi-user.target
 END
 
-systemctl daemon-reload
-systemctl enable ws-dropbear
-systemctl restart ws-dropbear
-
-#edusshssl
+# Installing Service ws-stunnel
 wget -O /usr/local/bin/ws-stunnel "https://raw.githubusercontent.com/syapik96/aws/main/lain2/ws-stunnel.py"
 chmod +x /usr/local/bin/ws-stunnel
 
-# Installing Service
-cat > /etc/systemd/system/ws-stunnel.service <<-END
+# Create system Service ws-stunnel
+cat > /etc/systemd/system/ws-stunnel.service <<END
 [Unit]
 Description=Python Ssl Proxy Websocket
-Documentation=https://gilergames.tk
+Documentation=https://github.com/syapik96/aws
 After=network.target nss-lookup.target
 
 [Service]
@@ -81,19 +74,15 @@ Restart=on-failure
 WantedBy=multi-user.target
 END
 
-systemctl daemon-reload
-systemctl enable ws-stunnel
-systemctl restart ws-stunnel
-
-#edu-openssh
+# Installing Service edu-openssh
 wget -O /usr/local/bin/edu-openssh "https://raw.githubusercontent.com/syapik96/aws/main/lain2/edu-openssh.py"
 chmod +x /usr/local/bin/edu-openssh
 
-# Installing Service
+# Create system Service edu-openssh
 cat > /etc/systemd/system/edu-openssh.service <<END
 [Unit]
 Description=Python Openssh Proxy Websocket
-Documentation=https://gilergames.tk
+Documentation=https://github.com/syapik96/aws
 After=network.target nss-lookup.target
 
 [Service]
@@ -109,6 +98,13 @@ Restart=on-failure
 WantedBy=multi-user.target
 END
 
+# ENABLE & START/RESTART SERVICE
 systemctl daemon-reload
+systemctl enable edu-ovpn
+systemctl restart edu-ovpn
+systemctl enable ws-dropbear
+systemctl restart ws-dropbear
+systemctl enable ws-stunnel
+systemctl restart ws-stunnel
 systemctl enable edu-openssh
 systemctl restart edu-openssh
