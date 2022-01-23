a2f3292fdac384b483ef8c3ea5cc665ff318d097
syapik96 <79759525+syapik96@users.noreply.github.com>
2022-01-24 04:46:03 +0800

    Update ceklim.sh

--- ceklim.sh
+++ ceklim.sh
@@ -1,21 +1,22 @@
 #!/bin/bash
+# Script mod updated by prince
+
 clear
-echo " "
-echo "===========================================";
-echo " ";
+echo -e " "
+echo -e " =========[ Check Limit Status ]========" | lolcat;
+echo -e " ";
 if [ -e "/root/log-limit.txt" ]; then
-echo "Pengguna Yang Melanggar Batas Maksimum";
-echo "Waktu - Username - Banyak Multilogin"
-echo "-------------------------------------";
+echo -e "  Pengguna Yang Melanggar Batas Maksimum" | lolcat;
+echo -e "   Waktu - Username - Banyak Multilogin " | lolcat
+echo -e " ---------------------------------------" | lolcat;
 cat /root/log-limit.txt
 else
-echo " Tidak ada pengguna yang melakukan pelanggaran"
-echo " "
-echo " atau"
-echo " "
-echo " Script User-Limit belum dijalankan"
+echo -e " \e[1;31mTidak ada pengguna yang melakukan pelanggaran\e[0m"
+echo -e " "
+echo -e " atau"
+echo -e " "
+echo -e " \e[1;31mScript User-Limit belum dijalankan\e[0m"
 fi
-echo " ";
-echo "===========================================";
-echo "Mod BY SL";
-echo " ";
+echo -e " ";
+echo -e " =======================================" | lolcat;
+echo -e " ";
