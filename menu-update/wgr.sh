10da918271e709c47ca6f35ff0301cdf9ed1f9ea
syapik96 <79759525+syapik96@users.noreply.github.com>
2022-01-28 01:47:46 +0800

    Update wgr.sh

--- menu-update/wgr.sh
+++ menu-update/wgr.sh
@@ -9,22 +9,22 @@ bd='\e[1m'
 clear
 figlet -f standard "Panel Wireguard" | lolcat
 echo -e ""
-echo -e " ╠═══════════════════════════════════════╣" | lolcat
-echo -e " ║            PANEL WIREGUARD            ║" | lolcat
-echo -e " ╠═══════════════════════════════════════╣" | lolcat
+echo -e " ╠════════════════════════════════╣" | lolcat
+echo -e " ║            PANEL WIREGUARD                          ║" | lolcat
+echo -e " ╠════════════════════════════════╣" | lolcat
 echo -e " $bd║\e[m"
 echo -e " $bd║\e[m 1$bl]\e[m$bd Create Wireguard Account"
 echo -e " $bd║\e[m 2$bl]\e[m$bd Delete Wireguard Account"
 echo -e " $bd║\e[m 3$bl]\e[m$bd Check User Login Wireguard"
 echo -e " $bd║\e[m 4$bl]\e[m$bd Renew Wireguard Account"
 echo -e " $bd║\e[m"
-echo -e " ╠═══════════════════════════════════════╣" | lolcat
-echo -e " ║  0]  RETURN  MENU                     ║"   | lolcat
-echo -e " ╠═══════════════════════════════════════╣" | lolcat
+echo -e " ╠═══════════════════════════════╣" | lolcat
+echo -e " ║  0]  RETURN  MENU                                  ║"   | lolcat
+echo -e " ╠═══════════════════════════════╣" | lolcat
 echo -e "\e[1;32m"
-read -p "     Please select an option :  "  wgr
+read -p "     Please select an option :  " wgg
 echo -e "\e[0m"
-case $wgr in
+case $wgg in
     1)
     clear
     addwg
@@ -53,8 +53,6 @@ case $wgr in
     *)
     echo -e "${red}Please enter an correct number${NC}"
     sleep 2
-    clear
     wgr
-    exit
     ;;
 esac
