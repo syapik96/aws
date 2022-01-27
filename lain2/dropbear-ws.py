3fedf5bb07a1436a4f3155988fc41e608cfc0edf
syapik96 <79759525+syapik96@users.noreply.github.com>
2022-01-28 01:51:09 +0800

    Update dropbear-ws.py

--- lain2/dropbear-ws.py
+++ lain2/dropbear-ws.py
@@ -3,10 +3,9 @@ import socket, threading, thread, select, signal, sys, time, getopt
 
 # Listen
 LISTENING_ADDR = '0.0.0.0'
-if sys.argv[1:]:
-  LISTENING_PORT = sys.argv[1]
-else:
-  LISTENING_PORT = 80  
+LISTENING_PORT = sys.argv[1]
+#else:
+#LISTENING_PORT = 80  
 #Pass
 PASS = ''
 
