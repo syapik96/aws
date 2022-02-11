0465be2c76b28986aadd5c6d1200a209b912ff3f
syapik96 <79759525+syapik96@users.noreply.github.com>
2022-02-12 01:39:10 +0800

    update sslh setup

--- install/ssh-vpn.sh
+++ install/ssh-vpn.sh
@@ -144,6 +144,40 @@ apt -y install squid3
 wget -O /etc/squid/squid.conf "https://raw.githubusercontent.com/${GitUser}/aws/main/squid3.conf"
 sed -i $MYIP2 /etc/squid/squid.conf
 
+#​ Install SSLH 
+apt -y install sslh 
+​rm -f /etc/default/sslh 
+  
+​#​ Settings SSLH 
+​cat ​>​ /etc/default/sslh ​<<​-​END 
+​# Default options for sslh initscript 
+​# sourced by /etc/init.d/sslh 
+  
+​# Disabled by default, to force yourself 
+# to read the configuration: 
+# - /usr/share/doc/sslh/README.Debian (quick start) 
+​# - /usr/share/doc/sslh/README, at "Configuration" section 
+​# - sslh(8) via "man sslh" for more configuration details. 
+# Once configuration ready, you *must* set RUN to yes here 
+# and try to start sslh (standalone mode only) 
+  
+RUN=yes 
+  
+# binary to use: forked (sslh) or single-thread (sslh-select) version 
+​# systemd users: don't forget to modify /lib/systemd/system/sslh.service 
+​DAEMON=/usr/sbin/sslh 
+
+DAEMON_OPTS="--user sslh --listen 0.0.0.0:443 --ssl 127.0.0.1:777 --ssh 127.0.0.1:109 --openvpn 127.0.0.1:1194 --http 127.0.0.1:8880 --pidfile /var/run/sslh/sslh.pid -n" 
+  
+END 
+  
+​#​ Restart Service SSLH 
+service sslh restart 
+systemctl restart sslh 
+/etc/init.d/sslh restart 
+​/etc/init.d/sslh status 
+​/etc/init.d/sslh restart
+
 # setting vnstat
 apt-get -y install vnstat
 /etc/init.d/vnstat restart
